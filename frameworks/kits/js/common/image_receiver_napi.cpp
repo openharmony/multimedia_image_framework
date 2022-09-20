18da5f9c0866a0cb5db2097d52341e61161bf671
renhw <renhongwei9@huawei.com>
2022-09-20 11:21:20 +0800

    fix AFBC
    Signed-off-by: renhw <renhongwei9@huawei.com>

--- frameworks/kits/js/common/image_receiver_napi.cpp
+++ frameworks/kits/js/common/image_receiver_napi.cpp
@@ -488,7 +488,7 @@ static void DoTest(std::shared_ptr<ImageReceiver> imageReceiver)
         .height = 0x100,
         .strideAlignment = 0x8,
         .format = PIXEL_FMT_RGBA_8888,
-        .usage = HBM_USE_CPU_READ | HBM_USE_CPU_WRITE | HBM_USE_MEM_DMA,
+        .usage = BUFFER_USAGE_CPU_READ| BUFFER_USAGE_CPU_WRITE | BUFFER_USAGE_MEM_DMA,
         .timeout = 0,
     };
 
