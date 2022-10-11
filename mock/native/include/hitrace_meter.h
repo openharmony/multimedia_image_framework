a98ccf223169c549c20e5c5d6d906b5076bd6d10
renhongwei <renhongwei9@huawei.com>
2022-10-11 15:58:23 +0800

    add image to accross Platform
    Signed-off-by: renhongwei <renhongwei9@huawei.com>

--- mock/native/include/hitrace_meter.h
+++ mock/native/include/hitrace_meter.h
@@ -1,12 +1,27 @@
-#ifndef INTERFACES_INNERKITS_NATIVE_HITRACE_METER_H
-#define INTERFACES_INNERKITS_NATIVE_HITRACE_METER_H
+/*
+ * Copyright (C) 2022 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef MOCK_NATIVE_INCLUDE_HILOG_HITRACE_METER_H
+#define MOCK_NATIVE_INCLUDE_HILOG_HITRACE_METER_H
 
 #include <string>
 
 #ifdef __cplusplus
 extern "C" {
 #endif
-#define HITRACE_TAG_ZIMAGE -1
+#define HITRACE_TAG_ZIMAGE (-1)
 /**
  * Track the beginning of a context.
  */
@@ -20,4 +35,4 @@ void FinishTrace(uint64_t label);
 #ifdef __cplusplus
 }
 #endif
-#endif // INTERFACES_INNERKITS_NATIVE_HITRACE_METER_H
+#endif // MOCK_NATIVE_INCLUDE_HILOG_HITRACE_METER_H
