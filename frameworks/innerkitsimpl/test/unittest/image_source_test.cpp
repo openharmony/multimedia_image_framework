54c814a0780c687348c6d2922a5a9ea3c895538c
zhangqiang183 <zhangqiang183@huawei.com>
2022-10-25 15:21:42 +0800

    ut
    Signed-off-by: zhangqiang183 <zhangqiang183@huawei.com>

--- frameworks/innerkitsimpl/test/unittest/image_source_test.cpp
+++ frameworks/innerkitsimpl/test/unittest/image_source_test.cpp
@@ -49,11 +49,289 @@ using namespace std;
 using namespace ImagePlugin;
 using namespace MultimediaPlugin;
 
+static const std::string IMAGE_INPUT_WEBP_PATH = "/data/local/tmp/image/test_large.webp";
+
 class ImageSourceTest : public testing::Test {
 public:
     ImageSourceTest() {}
     ~ImageSourceTest() {}
 };
+
+/**
+ * @tc.name: GetSupportedFormats001
+ * @tc.desc: test GetSupportedFormats
+ * @tc.type: FUNC
+ */
+HWTEST_F(ImageSourceTest, GetSupportedFormats001, TestSize.Level3)
+{
+    GTEST_LOG_(INFO) << "ImageSourceTest: GetSupportedFormats001 start";
+
+    uint32_t errorCode = 0;
+    SourceOptions opts;
+    opts.formatHint = "image/jpeg";
+    std::unique_ptr<ImageSource> imageSource = ImageSource::CreateImageSource(IMAGE_INPUT_JPEG_PATH, opts, errorCode);
+    ASSERT_EQ(errorCode, SUCCESS);
+    ASSERT_NE(imageSource.get(), nullptr);
+    std::set<std::string> formats;
+    uint32_t ret = imageSource->GetSupportedFormats(formats);
+    ASSERT_EQ(ret, SUCCESS);
+
+    GTEST_LOG_(INFO) << "ImageSourceTest: GetSupportedFormats001 end";
+}
+
+/**
+ * @tc.name: CreateIncrementalPixelMap001
+ * @tc.desc: test CreateIncrementalPixelMap
+ * @tc.type: FUNC
+ */
+HWTEST_F(ImageSourceTest, CreateIncrementalPixelMap001, TestSize.Level3)
+{
+    GTEST_LOG_(INFO) << "ImageSourceTest: CreateIncrementalPixelMap001 start";
+
+    size_t bufferSize = 0;
+    bool fileRet = ImageUtils::GetFileSize(IMAGE_INPUT_JPEG_PATH, bufferSize);
+    ASSERT_EQ(fileRet, true);
+    uint8_t *buffer = reinterpret_cast<uint8_t *>(malloc(bufferSize));
+    ASSERT_NE(buffer, nullptr);
+    fileRet = OHOS::ImageSourceUtil::ReadFileToBuffer(IMAGE_INPUT_JPEG_PATH, buffer, bufferSize);
+    ASSERT_EQ(fileRet, true);
+    uint32_t errorCode = 0;
+    IncrementalSourceOptions incOpts;
+    incOpts.incrementalMode = IncrementalMode::INCREMENTAL_DATA;
+    std::unique_ptr<ImageSource> imageSource = ImageSource::CreateIncrementalImageSource(incOpts, errorCode);
+    ASSERT_EQ(errorCode, SUCCESS);
+
+    DecodeOptions decodeOpts;
+    std::unique_ptr<IncrementalPixelMap> incPixelMap = imageSource->CreateIncrementalPixelMap(0, decodeOpts,
+        errorCode);
+    ASSERT_NE(incPixelMap, nullptr);
+
+    GTEST_LOG_(INFO) << "ImageSourceTest: CreateIncrementalPixelMap001 end";
+}
+
+/**
+ * @tc.name: UpdateData001
+ * @tc.desc: test UpdateData
+ * @tc.type: FUNC
+ */
+HWTEST_F(ImageSourceTest, UpdateData001, TestSize.Level3)
+{
+    GTEST_LOG_(INFO) << "ImageSourceTest: UpdateData001 start";
+
+    size_t bufferSize = 0;
+    bool fileRet = ImageUtils::GetFileSize("/data/local/tmp/image/moving_test.gif", bufferSize);
+    ASSERT_EQ(fileRet, true);
+    uint8_t *buffer = reinterpret_cast<uint8_t *>(malloc(bufferSize));
+    ASSERT_NE(buffer, nullptr);
+    fileRet = ReadFileToBuffer("/data/local/tmp/image/moving_test.gif", buffer, bufferSize);
+    ASSERT_EQ(fileRet, true);
+    uint32_t errorCode = 0;
+    IncrementalSourceOptions incOpts;
+    incOpts.incrementalMode = IncrementalMode::INCREMENTAL_DATA;
+    std::unique_ptr<ImageSource> imageSource = ImageSource::CreateIncrementalImageSource(incOpts, errorCode);
+    ASSERT_EQ(errorCode, SUCCESS);
+    ASSERT_NE(imageSource.get(), nullptr);
+    
+    DecodeOptions decodeOpts;
+    std::unique_ptr<IncrementalPixelMap> incPixelMap = imageSource->CreateIncrementalPixelMap(0, decodeOpts, errorCode);
+    uint32_t updateSize = 0;
+    bool isCompleted = false;
+    while (updateSize < bufferSize) {
+        uint32_t updateOnceSize = 10240;
+        if (updateSize + updateOnceSize > bufferSize) {
+            updateOnceSize = bufferSize - updateSize;
+            isCompleted = true;
+        }
+        uint32_t ret = imageSource->UpdateData(buffer + updateSize, updateOnceSize, isCompleted);
+        ASSERT_EQ(ret, SUCCESS);
+    }
+
+    GTEST_LOG_(INFO) << "ImageSourceTest: UpdateData001 end";
+}
+
+/**
+ * @tc.name: GetImageInfo001
+ * @tc.desc: test GetImageInfo
+ * @tc.type: FUNC
+ */
+HWTEST_F(ImageSourceTest, GetImageInfo001, TestSize.Level3)
+{
+    GTEST_LOG_(INFO) << "ImageSourceTest: GetImageInfo001 start";
+
+    uint32_t errorCode = 0;
+    DecodeOptions decodeOpts;
+    std::unique_ptr<PixelMap> pixelMap = imageSource->CreatePixelMap(decodeOpts, errorCode);
+    ASSERT_EQ(errorCode, SUCCESS);
+    ASSERT_NE(pixelMap.get(), nullptr);
+
+    ImageInfo imageInfo;
+    pixelMap->GetImageInfo(imageInfo);
+    decodeOpts.CropRect = { imageInfo.size.width - 1, imageInfo.size.height - 1, 1, 1 };
+    std::unique_ptr<PixelMap> cropPixelMap = imageSource->CreatePixelMap(decodeOpts, errorCode);
+    ASSERT_NE(pixelMap, nullptr);
+    ASSERT_NE(pixelMap.get(), nullptr);
+    cropPixelMap->GetImageInfo(imageInfo);
+    ASSERT_EQ(imageInfo.size.width, 1);
+    ASSERT_EQ(imageInfo.size.height, 1);
+
+    GTEST_LOG_(INFO) << "ImageSourceTest: GetImageInfo001 end";
+}
+
+/**
+ * @tc.name: GetSourceInfo001
+ * @tc.desc: test GetSourceInfo
+ * @tc.type: FUNC
+ */
+HWTEST_F(ImageSourceTest, GetSourceInfo001, TestSize.Level3)
+{
+    GTEST_LOG_(INFO) << "ImageSourceTest: GetSourceInfo001 start";
+
+    uint32_t errorCode = 0;
+    SourceOptions opts;
+    std::unique_ptr<ImageSource> imageSource = ImageSource::CreateImageSource(IMAGE_INPUT_WEBP_PATH, opts, errorCode);
+
+    DecodeOptions decodeOpts;
+    std::unique_ptr<PixelMap> pixelMap = imageSource->CreatePixelMap(decodeOpts, errorCode);
+    ASSERT_EQ(errorCode, SUCCESS);
+
+    SourceInfo sourceInfo = imageSource->GetSourceInfo(errorCode);
+    ASSERT_EQ(errorCode, SUCCESS);
+
+    GTEST_LOG_(INFO) << "ImageSourceTest: GetSourceInfo001 end";
+}
+
+/**
+ * @tc.name: RegisterListener001
+ * @tc.desc: test RegisterListener
+ * @tc.type: FUNC
+ */
+HWTEST_F(ImageSourceTest, RegisterListener001, TestSize.Level3)
+{
+    GTEST_LOG_(INFO) << "ImageSourceTest: RegisterListener001 start";
+
+    PeerListener *listener = nullptr;
+    uint32_t errorCode = 0;
+    SourceOptions opts;
+    std::unique_ptr<ImageSource> imageSource = ImageSource::CreateImageSource(IMAGE_INPUT_WEBP_PATH, opts, errorCode);
+    imageSource->RegisterListener(listener);
+
+    GTEST_LOG_(INFO) << "ImageSourceTest: RegisterListener001 end";
+}
+
+/**
+ * @tc.name: UnRegisterListener001
+ * @tc.desc: test UnRegisterListener
+ * @tc.type: FUNC
+ */
+HWTEST_F(ImageSourceTest, UnRegisterListener001, TestSize.Level3)
+{
+    GTEST_LOG_(INFO) << "ImageSourceTest: UnRegisterListener001 start";
+
+    PeerListener *listener = nullptr;
+    uint32_t errorCode = 0;
+    SourceOptions opts;
+    std::unique_ptr<ImageSource> imageSource = ImageSource::CreateImageSource(IMAGE_INPUT_WEBP_PATH, opts, errorCode);
+    imageSource->UnRegisterListener(listener);
+
+    GTEST_LOG_(INFO) << "ImageSourceTest: UnRegisterListener001 end";
+}
+
+/**
+ * @tc.name: GetDecodeEvent001
+ * @tc.desc: test GetDecodeEvent
+ * @tc.type: FUNC
+ */
+HWTEST_F(ImageSourceTest, GetDecodeEvent001, TestSize.Level3)
+{
+    GTEST_LOG_(INFO) << "ImageSourceTest: GetDecodeEvent001 start";
+
+    uint32_t errorCode = 0;
+    SourceOptions opts;
+    std::unique_ptr<ImageSource> imageSource = ImageSource::CreateImageSource(IMAGE_INPUT_WEBP_PATH, opts, errorCode);
+    DecodeEvent decodeEvent_ = DecodeEvent::EVENT_COMPLETE_DECODE;
+    decodeEvent_ = imageSource->GetDecodeEvent();
+
+    GTEST_LOG_(INFO) << "ImageSourceTest: GetDecodeEvent001 end";
+}
+
+/**
+ * @tc.name: AddDecodeListener001
+ * @tc.desc: test AddDecodeListener
+ * @tc.type: FUNC
+ */
+HWTEST_F(ImageSourceTest, AddDecodeListener001, TestSize.Level3)
+{
+    GTEST_LOG_(INFO) << "ImageSourceTest: AddDecodeListener001 start";
+
+    DecodeListener *listener = nullptr;
+    uint32_t errorCode = 0;
+    SourceOptions opts;
+    std::unique_ptr<ImageSource> imageSource = ImageSource::CreateImageSource(IMAGE_INPUT_WEBP_PATH, opts, errorCode);
+    imageSource->AddDecodeListener(listener);
+
+    GTEST_LOG_(INFO) << "ImageSourceTest: AddDecodeListener001 end";
+}
+
+/**
+ * @tc.name: RemoveDecodeListener001
+ * @tc.desc: test RemoveDecodeListener
+ * @tc.type: FUNC
+ */
+HWTEST_F(ImageSourceTest, RemoveDecodeListener001, TestSize.Level3)
+{
+    GTEST_LOG_(INFO) << "ImageSourceTest: RemoveDecodeListener001 start";
+
+    DecodeListener *listener = nullptr;
+    uint32_t errorCode = 0;
+    SourceOptions opts;
+    std::unique_ptr<ImageSource> imageSource = ImageSource::CreateImageSource(IMAGE_INPUT_WEBP_PATH, opts, errorCode);
+    imageSource->RemoveDecodeListener(listener);
+
+    GTEST_LOG_(INFO) << "ImageSourceTest: RemoveDecodeListener001 end";
+}
+
+/**
+ * @tc.name: IsIncrementalSource001
+ * @tc.desc: test IsIncrementalSource
+ * @tc.type: FUNC
+ */
+HWTEST_F(ImageSourceTest, IsIncrementalSource001, TestSize.Level3)
+{
+    GTEST_LOG_(INFO) << "ImageSourceTest: IsIncrementalSource001 start";
+
+    bool isIncrementalSource_ = false;
+    uint32_t errorCode = 0;
+    SourceOptions opts;
+    std::unique_ptr<ImageSource> imageSource = ImageSource::CreateImageSource(IMAGE_INPUT_WEBP_PATH, opts, errorCode);
+    isIncrementalSource_ = imageSource->IsIncrementalSource();
+
+    GTEST_LOG_(INFO) << "ImageSourceTest: IsIncrementalSource001 end";
+}
+
+/**
+ * @tc.name: GetImagePropertyInt001
+ * @tc.desc: test GetImagePropertyInt
+ * @tc.type: FUNC
+ */
+HWTEST_F(ImageSourceTest, GetImagePropertyInt001, TestSize.Level3)
+{
+    GTEST_LOG_(INFO) << "ImageSourceTest: GetImagePropertyInt001 start";
+
+    uint32_t errorCode = 0;
+    SourceOptions opts;
+    std::unique_ptr<ImageSource> imageSource = ImageSource::CreateImageSource(IMAGE_INPUT_JPEG_PATH, opts, errorCode);
+    ASSERT_EQ(errorCode, SUCCESS);
+    ASSERT_NE(imageSource.get(), nullptr);
+
+    uint32_t index = 0;
+    int32_t value = 0;
+    std::string key;
+    uint32_t ret = imageSource->GetImagePropertyInt(index, key, value);
+
+    ASSERT_EQ(ret, SUCCESS);
+
+    GTEST_LOG_(INFO) << "ImageSourceTest: GetImagePropertyInt001 end";
+}
 
 /**
  * @tc.name: ModifyImageProperty001
