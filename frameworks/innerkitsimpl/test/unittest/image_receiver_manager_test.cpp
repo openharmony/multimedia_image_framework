ec11dd53c6747702fd2ff12cf5d44ac8c033909a
renhongwei <renhongwei9@huawei.com>
2022-11-16 11:21:40 +0800

    add image receiver uttest
    Signed-off-by: renhongwei <renhongwei9@huawei.com>

---
+++ frameworks/innerkitsimpl/test/unittest/image_receiver_manager_test.cpp
@@ -0,0 +1,66 @@
+/*
+ * Copyright (C) 2021 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+ 
+#include <gtest/gtest.h>
+#include <fstream>
+#include <surface.h>
+#include "image_receiver.h"
+#include "image_packer.h"
+#include "image_source.h"
+#include "image_utils.h"
+#include "hilog/log.h"
+#include "image_receiver_manager.h"
+
+using namespace testing::ext;
+using namespace OHOS::Media;
+using namespace std;
+namespace OHOS {
+namespace Multimedia {
+class ImageReceiverManagerTest : public testing::Test {
+public:
+    ImageReceiverManagerTest() {}
+    ~ImageReceiverManagerTest() {}
+};
+
+/**
+ * @tc.name: ImageReceiverManager001
+ * @tc.desc: test SaveImageReceiver
+ * @tc.type: FUNC
+ */
+HWTEST_F(ImageReceiverManagerTest, ImageReceiverManager001, TestSize.Level3)
+{
+    GTEST_LOG_(INFO) << "ImageReceiverManagerTest: ImageReceiverManager001 start";
+    std::shared_ptr<ImageReceiver> iva = std::make_shared<ImageReceiver>();
+    ImageReceiverManager& imageReceiverManager = ImageReceiverManager::getInstance();
+    std::string receiverKey = imageReceiverManager.SaveImageReceiver(iva);
+    ASSERT_EQ(receiverKey, "1");
+    GTEST_LOG_(INFO) << "ImageReceiverManagerTest: ImageReceiverManager001 end";
+}
+
+/**
+ * @tc.name: ImageReceiverManager002
+ * @tc.desc: test getImageReceiverByKeyId
+ * @tc.type: FUNC
+ */
+HWTEST_F(ImageReceiverManagerTest, ImageReceiverManager002, TestSize.Level3)
+{
+    GTEST_LOG_(INFO) << "ImageReceiverManagerTest: ImageReceiverManager002 start";
+    std::string id = "1";
+    ImageReceiverManager& imageReceiverManager = ImageReceiverManager::getInstance();
+    std::shared_ptr<ImageReceiver> imaReceive = imageReceiverManager.getImageReceiverByKeyId(id);
+    GTEST_LOG_(INFO) << "ImageReceiverManagerTest: ImageReceiverManager002 end";
+}
+}
+}
