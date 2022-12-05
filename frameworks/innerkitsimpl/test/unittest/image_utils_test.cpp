43adf509a7aedeadb4bbab438cab3877d3b684ad
冯泽悟 <fengzewu@huawei.com>
2022-12-05 10:07:50 +0000

    update frameworks/innerkitsimpl/test/unittest/image_utils_test.cpp.
    Signed-off-by: 冯泽悟 <fengzewu@huawei.com>

--- frameworks/innerkitsimpl/test/unittest/image_utils_test.cpp
+++ frameworks/innerkitsimpl/test/unittest/image_utils_test.cpp
@@ -16,7 +16,6 @@
 #include <gtest/gtest.h>
 #include <fcntl.h>
 #include <fstream>
-#include <securec.h>
 #include "image_utils.h"
 #include "image_trace.h"
 #include "source_stream.h"
@@ -199,15 +198,13 @@ HWTEST_F(ImageUtilsTest, PathToRealPath001, TestSize.Level3)
 HWTEST_F(ImageUtilsTest, PathToRealPath002, TestSize.Level3)
 {
     GTEST_LOG_(INFO) << "ImageUtilsTest: PathToRealPath002 start";
-    char *buffer = (char*)malloc(PATH_MAX+1);
-    memset_s(buffer, sizeof(PATH_MAX+1), 0, sizeof(PATH_MAX+1));
+    char buffer[PATH_MAX+1] = {'\0'};
     for (int i = 0; i <= PATH_MAX; i++) {
         buffer[i] = i;
     }
     const string path = buffer;
     string realPath;
     ImageUtils::PathToRealPath(path, realPath);
-    free(buffer);
     GTEST_LOG_(INFO) << "ImageUtilsTest: PathToRealPath002 end";
 }
 
