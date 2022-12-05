8b1b4c6a6c22edebae76e7a88043dfaf7f26c6c1
冯泽悟 <fengzewu@huawei.com>
2022-12-05 07:30:50 +0000

    update frameworks/innerkitsimpl/test/unittest/image_utils_test.cpp.
    Signed-off-by: 冯泽悟 <fengzewu@huawei.com>

--- frameworks/innerkitsimpl/test/unittest/image_utils_test.cpp
+++ frameworks/innerkitsimpl/test/unittest/image_utils_test.cpp
@@ -200,9 +200,8 @@ HWTEST_F(ImageUtilsTest, PathToRealPath002, TestSize.Level3)
 {
     GTEST_LOG_(INFO) << "ImageUtilsTest: PathToRealPath002 start";
     char *buffer = (char*)malloc(PATH_MAX+1);
-    if (memset_s(buffer, sizeof(buffer), 0, sizeof(PATH_MAX+1)) != EOK) {
-        ASSERT_NE(*buffer, 0);
-    }
+    ASSERT_NE(buffer, nullptr);
+    (void)memset_s(buffer, PATH_MAX+1, 0, PATH_MAX+1);
     for (int i = 0; i <= PATH_MAX; i++) {
         buffer[i] = i;
     }
