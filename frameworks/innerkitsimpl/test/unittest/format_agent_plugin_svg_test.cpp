38bc88df0257ed26f1b8deb66c5e2097820f07c8
fengzewu <fengzewu@huawei.com>
2023-01-30 11:40:33 +0800

    fix format_agent_plugin_svg_test.cpp
    Signed-off-by: fengzewu <fengzewu@huawei.com>

--- frameworks/innerkitsimpl/test/unittest/format_agent_plugin_svg_test.cpp
+++ frameworks/innerkitsimpl/test/unittest/format_agent_plugin_svg_test.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2022 Huawei Device Co., Ltd.
+ * Copyright (C) 2021 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -14,120 +14,136 @@
  */
 
 #include <gtest/gtest.h>
-#include "hilog/log.h"
-#include "image_source_util.h"
-#include "log_tags.h"
+#include "image_source.h"
 #include "media_errors.h"
+#include "svg_format_agent.h"
 
 using namespace testing::ext;
 using namespace OHOS::Media;
-using namespace OHOS::HiviewDFX;
-using namespace OHOS::ImageSourceUtil;
 
 namespace OHOS {
 namespace Multimedia {
-namespace {
-static constexpr HiLogLabel LABEL_TEST = { LOG_CORE, LOG_TAG_DOMAIN_ID_IMAGE, "ImageSourceSvgTest" };
+static const std::string IMAGE_INPUT_SVG_PATH = "/data/local/tmp/image/test.svg";
 static const std::string SVG_FORMAT_TYPE = "image/svg+xml";
-static const std::string INPUT_PATH = "/data/local/tmp/image/";
-static const std::string OUTPUT_PATH = "/data/local/tmp/image/output_";
-static const std::string OUTPUT_EXT = ".jpg";
-static const std::string TEST_FILE_SVG = "test.svg";
-static const std::string TEST_FILE_LARGE_SVG = "test_large.svg";
-}
+static constexpr uint8_t SVG_HEADER[] = { '<', '?', 'x', 'm', 'l' };
 
-class ImageSourceSvgTest : public testing::Test {
+class FormatAgentPluginSvgTest : public testing::Test {
 public:
-    ImageSourceSvgTest() {}
-    ~ImageSourceSvgTest() {}
+    FormatAgentPluginSvgTest() {}
+    ~FormatAgentPluginSvgTest() {}
 };
 
 /**
- * @tc.name: SvgImageDecode001
- * @tc.desc: Decode svg image from file source stream
+ * @tc.name: SvgFormatAgentPluginTest001
+ * @tc.desc: svg GetFormatType
  * @tc.type: FUNC
  */
-HWTEST_F(ImageSourceSvgTest, SvgImageDecode001, TestSize.Level3)
+HWTEST_F(FormatAgentPluginSvgTest, SvgFormatAgentPluginTest001, TestSize.Level3)
 {
-    GTEST_LOG_(INFO) << "ImageSourceSvgTest: SvgImageDecode001 start";
-
-    const std::string testName = TEST_FILE_SVG;
-
-    /**
-     * @tc.steps: step1. create image source by correct svg file path and svg format hit.
-     * @tc.expected: step1. create image source success.
-     */
-    uint32_t errorCode = 0;
-    SourceOptions opts;
-    opts.formatHint = SVG_FORMAT_TYPE;
-    const std::string inName = INPUT_PATH + testName;
-    auto imageSource = ImageSource::CreateImageSource(inName, opts, errorCode);
-    ASSERT_EQ(errorCode, SUCCESS);
-    ASSERT_NE(imageSource.get(), nullptr);
-
-    /**
-     * @tc.steps: step2. decode image source to pixel map by default decode options
-     * @tc.expected: step2. decode image source to pixel map success.
-     */
-    DecodeOptions decodeOpts;
-    auto pixelMap = imageSource->CreatePixelMap(decodeOpts, errorCode);
-    HiLog::Debug(LABEL_TEST, "create pixel map error code=%{public}u.", errorCode);
-    ASSERT_EQ(errorCode, SUCCESS);
-    ASSERT_NE(pixelMap.get(), nullptr);
-
-    /**
-     * @tc.steps: step3. compress the pixel map to jpg file.
-     * @tc.expected: step3. pack pixel map success.
-     */
-    const std::string outName = OUTPUT_PATH + testName + OUTPUT_EXT;
-    auto packSize = PackImage(outName, std::move(pixelMap));
-    ASSERT_NE(packSize, 0);
-
-    GTEST_LOG_(INFO) << "ImageSourceSvgTest: SvgImageDecode001 end";
+    GTEST_LOG_(INFO) << "FormatAgentPluginSvgTest: SvgFormatAgentPluginTest001 start";
+    ImagePlugin::SvgFormatAgent formatAgent;
+    auto ret = formatAgent.GetFormatType();
+    ASSERT_EQ(ret, SVG_FORMAT_TYPE);
+    GTEST_LOG_(INFO) << "FormatAgentPluginSvgTest: SvgFormatAgentPluginTest001 end";
 }
 
 /**
- * @tc.name: SvgImageDecode002
- * @tc.desc: Decode svg image from file source stream
+ * @tc.name: SvgFormatAgentPluginTest002
+ * @tc.desc: svg GetHeaderSize
  * @tc.type: FUNC
  */
-HWTEST_F(ImageSourceSvgTest, SvgImageDecode002, TestSize.Level3)
+HWTEST_F(FormatAgentPluginSvgTest, SvgFormatAgentPluginTest002, TestSize.Level3)
 {
-    GTEST_LOG_(INFO) << "ImageSourceSvgTest: SvgImageDecode002 start";
+    GTEST_LOG_(INFO) << "FormatAgentPluginSvgTest: SvgFormatAgentPluginTest002 start";
+    ImagePlugin::SvgFormatAgent formatAgent;
+    auto ret = formatAgent.GetHeaderSize();
+    ASSERT_EQ(ret, sizeof(SVG_HEADER));
+    GTEST_LOG_(INFO) << "FormatAgentPluginSvgTest: SvgFormatAgentPluginTest002 end";
+}
 
-    const std::string testName = TEST_FILE_LARGE_SVG;
+/**
+ * @tc.name: SvgFormatAgentPluginTest003
+ * @tc.desc: svg CheckFormat
+ * @tc.type: FUNC
+ */
+HWTEST_F(FormatAgentPluginSvgTest, SvgFormatAgentPluginTest003, TestSize.Level3)
+{
+    GTEST_LOG_(INFO) << "FormatAgentPluginSvgTest: SvgFormatAgentPluginTest003 start";
+    ImagePlugin::SvgFormatAgent formatAgent;
+    auto datasize = formatAgent.GetHeaderSize();
+    void *headerData = nullptr;
+    auto ret = formatAgent.CheckFormat(headerData, datasize);
+    ASSERT_EQ(ret, false);
+    GTEST_LOG_(INFO) << "FormatAgentPluginSvgTest: SvgFormatAgentPluginTest003 end";
+}
 
-    /**
-     * @tc.steps: step1. create image source by correct svg file path and svg format hit.
-     * @tc.expected: step1. create image source success.
-     */
+/**
+ * @tc.name: SvgFormatAgentPluginTest004
+ * @tc.desc: svg CheckFormat
+ * @tc.type: FUNC
+ */
+HWTEST_F(FormatAgentPluginSvgTest, SvgFormatAgentPluginTest004, TestSize.Level3)
+{
+    GTEST_LOG_(INFO) << "FormatAgentPluginSvgTest: SvgFormatAgentPluginTest004 start";
+    ImagePlugin::SvgFormatAgent formatAgent;
+    auto datasize = formatAgent.GetHeaderSize();
     uint32_t errorCode = 0;
     SourceOptions opts;
     opts.formatHint = SVG_FORMAT_TYPE;
-    const std::string inName = INPUT_PATH + testName;
-    auto imageSource = ImageSource::CreateImageSource(inName, opts, errorCode);
+    auto imageSource = ImageSource::CreateImageSource(IMAGE_INPUT_SVG_PATH, opts, errorCode);
     ASSERT_EQ(errorCode, SUCCESS);
     ASSERT_NE(imageSource.get(), nullptr);
 
-    /**
-     * @tc.steps: step2. decode image source to pixel map by default decode options
-     * @tc.expected: step2. decode image source to pixel map success.
-     */
     DecodeOptions decodeOpts;
     auto pixelMap = imageSource->CreatePixelMap(decodeOpts, errorCode);
-    HiLog::Debug(LABEL_TEST, "create pixel map error code=%{public}u.", errorCode);
     ASSERT_EQ(errorCode, SUCCESS);
     ASSERT_NE(pixelMap.get(), nullptr);
-
-    /**
-     * @tc.steps: step3. compress the pixel map to jpg file.
-     * @tc.expected: step3. pack pixel map success.
-     */
-    const std::string outName = OUTPUT_PATH + testName + OUTPUT_EXT;
-    auto packSize = PackImage(outName, std::move(pixelMap));
-    ASSERT_NE(packSize, 0);
-
-    GTEST_LOG_(INFO) << "ImageSourceSvgTest: SvgImageDecode002 end";
+    
+    auto ret = formatAgent.CheckFormat(pixelMap->GetPixels(), datasize);
+    ASSERT_EQ(ret, false);
+    GTEST_LOG_(INFO) << "FormatAgentPluginSvgTest: SvgFormatAgentPluginTest004 end";
 }
-} // namespace Multimedia
-} // namespace OHOS
+
+/**
+ * @tc.name: SvgFormatAgentPluginTest005
+ * @tc.desc: svg CheckFormat
+ * @tc.type: FUNC
+ */
+HWTEST_F(FormatAgentPluginSvgTest, SvgFormatAgentPluginTest005, TestSize.Level3)
+{
+    GTEST_LOG_(INFO) << "FormatAgentPluginSvgTest: SvgFormatAgentPluginTest005 start";
+    ImagePlugin::SvgFormatAgent formatAgent;
+    auto datasize = formatAgent.GetHeaderSize();
+    auto ret = formatAgent.CheckFormat(nullptr, datasize);
+    ASSERT_EQ(ret, false);
+    GTEST_LOG_(INFO) << "FormatAgentPluginSvgTest: SvgFormatAgentPluginTest005 end";
+}
+
+/**
+ * @tc.name: SvgFormatAgentPluginTest006
+ * @tc.desc: svg CheckFormat
+ * @tc.type: FUNC
+ */
+HWTEST_F(FormatAgentPluginSvgTest, SvgFormatAgentPluginTest006, TestSize.Level3)
+{
+    GTEST_LOG_(INFO) << "FormatAgentPluginSvgTest: SvgFormatAgentPluginTest006 start";
+    ImagePlugin::SvgFormatAgent formatAgent;
+    auto datasize = formatAgent.GetHeaderSize() - 10;
+    uint32_t errorCode = 0;
+    SourceOptions opts;
+    opts.formatHint = SVG_FORMAT_TYPE;
+    auto imageSource = ImageSource::CreateImageSource(IMAGE_INPUT_SVG_PATH, opts, errorCode);
+    ASSERT_EQ(errorCode, SUCCESS);
+    ASSERT_NE(imageSource.get(), nullptr);
+
+    DecodeOptions decodeOpts;
+    auto pixelMap = imageSource->CreatePixelMap(decodeOpts, errorCode);
+    ASSERT_EQ(errorCode, SUCCESS);
+    ASSERT_NE(pixelMap.get(), nullptr);
+    
+    auto ret = formatAgent.CheckFormat(pixelMap->GetPixels(), datasize);
+    ASSERT_EQ(ret, false);
+    GTEST_LOG_(INFO) << "FormatAgentPluginSvgTest: SvgFormatAgentPluginTest006 end";
+}
+}
+}
