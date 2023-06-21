aae471b4c03e85d380dd6bb919c41ee3c6c08fe7
fengzewu <fengzewu@huawei.com>
2023-06-21 23:20:33 +0800

    add ext plugin
    Signed-off-by: fengzewu <fengzewu@huawei.com>

---
+++ plugins/common/libs/image/libextplugin/src/ext_encoder.cpp
@@ -0,0 +1,161 @@
+/*
+ * Copyright (C) 2023 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "ext_encoder.h"
+#include <algorithm>
+#include <map>
+
+#include "SkBitmap.h"
+#include "SkImageEncoder.h"
+#ifdef IMAGE_COLORSPACE_FLAG
+#include "color_space.h"
+#endif
+#include "ext_pixel_convert.h"
+#include "ext_wstream.h"
+#include "image_type_converter.h"
+#include "image_utils.h"
+#include "media_errors.h"
+#include "string_ex.h"
+
+namespace OHOS {
+namespace ImagePlugin {
+using namespace OHOS::HiviewDFX;
+using namespace Media;
+namespace {
+    constexpr OHOS::HiviewDFX::HiLogLabel LABEL = {LOG_CORE, LOG_TAG_DOMAIN_ID_PLUGIN, "ExtEncoder"};
+}
+static const std::map<SkEncodedImageFormat, std::string> FORMAT_NAME = {
+    {SkEncodedImageFormat::kBMP, "image/bmp"},
+    {SkEncodedImageFormat::kGIF, "image/gif"},
+    {SkEncodedImageFormat::kICO, "image/ico"},
+    {SkEncodedImageFormat::kJPEG, "image/jpeg"},
+    {SkEncodedImageFormat::kPNG, "image/png"},
+    {SkEncodedImageFormat::kWBMP, "image/bmp"},
+    {SkEncodedImageFormat::kWEBP, "image/webp"},
+    {SkEncodedImageFormat::kPKM, ""},
+    {SkEncodedImageFormat::kKTX, ""},
+    {SkEncodedImageFormat::kASTC, ""},
+    {SkEncodedImageFormat::kDNG, ""},
+    {SkEncodedImageFormat::kHEIF, "image/heif"},
+};
+
+ExtEncoder::ExtEncoder()
+{
+}
+
+ExtEncoder::~ExtEncoder()
+{
+}
+
+uint32_t ExtEncoder::StartEncode(OutputDataStream &outputStream, PlEncodeOptions &option)
+{
+    output_ = &outputStream;
+    opts_ = option;
+    return SUCCESS;
+}
+
+uint32_t ExtEncoder::AddImage(Media::PixelMap &pixelMap)
+{
+    pixelmap_ = &pixelMap;
+    return SUCCESS;
+}
+
+struct TmpBufferHolder {
+    std::unique_ptr<uint8_t[]> buf = nullptr;
+};
+
+static SkImageInfo ToSkInfo(Media::PixelMap *pixelMap)
+{
+    ImageInfo info;
+    pixelMap->GetImageInfo(info);
+    SkColorType colorType = ImageTypeConverter::ToSkColorType(info.pixelFormat);
+    SkAlphaType alphaType = ImageTypeConverter::ToSkAlphaType(info.alphaType);
+    sk_sp<SkColorSpace> colorSpace = nullptr;
+#ifdef IMAGE_COLORSPACE_FLAG
+    if (pixelMap->InnerGetGrColorSpacePtr() != nullptr) {
+        colorSpace = pixelMap->InnerGetGrColorSpacePtr()->ToSkColorSpace();
+    }
+#endif
+    return SkImageInfo::Make(info.size.width, info.size.height, colorType, alphaType, colorSpace);
+}
+
+static uint32_t RGBToRGBx(Media::PixelMap *pixelMap, SkImageInfo &skInfo, TmpBufferHolder &holder)
+{
+    holder.buf = std::make_unique<uint8_t[]>(skInfo.computeMinByteSize());
+    ExtPixels src = {
+        static_cast<uint8_t*>(pixelMap->GetWritablePixels()),
+        pixelMap->GetCapacity(), pixelMap->GetWidth()*pixelMap->GetHeight(),
+    };
+    ExtPixels dst = {
+        holder.buf.get(), skInfo.computeMinByteSize(), skInfo.width()*skInfo.height(),
+    };
+    return ExtPixelConvert::RGBToRGBx(src, dst);
+}
+
+static uint32_t BuildSkBitmap(Media::PixelMap *pixelMap, SkBitmap &bitmap,
+    SkEncodedImageFormat format, TmpBufferHolder &holder)
+{
+    uint32_t res = SUCCESS;
+    SkImageInfo skInfo = ToSkInfo(pixelMap);
+    auto pixels = pixelMap->GetWritablePixels();
+    if (format == SkEncodedImageFormat::kJPEG &&
+        skInfo.colorType() == SkColorType::kRGB_888x_SkColorType &&
+        pixelMap->GetCapacity() < skInfo.computeMinByteSize()) {
+        res = RGBToRGBx(pixelMap, skInfo, holder);
+        if (res != SUCCESS) {
+            HiLog::Error(LABEL, "ExtEncoder::BuildSkBitmap pixel convert failed %{public}d", res);
+            return res;
+        }
+        pixels = holder.buf.get();
+        skInfo = skInfo.makeColorType(SkColorType::kRGBA_8888_SkColorType);
+    }
+
+    if (!bitmap.installPixels(skInfo, pixels, skInfo.minRowBytes())) {
+        HiLog::Error(LABEL, "ExtEncoder::BuildSkBitmap to skbitmap failed");
+        return ERR_IMAGE_INVALID_PARAMETER;
+    }
+    return res;
+}
+
+uint32_t ExtEncoder::FinalizeEncode()
+{
+    if (pixelmap_ == nullptr || output_ == nullptr) {
+        return ERR_IMAGE_INVALID_PARAMETER;
+    }
+    auto iter = std::find_if(FORMAT_NAME.begin(), FORMAT_NAME.end(),
+        [this](const std::map<SkEncodedImageFormat, std::string>::value_type item) {
+            return IsSameTextStr(item.second, opts_.format);
+    });
+    if (iter == FORMAT_NAME.end()) {
+        HiLog::Error(LABEL, "ExtEncoder::FinalizeEncode unsupported format %{public}s", opts_.format.c_str());
+        return ERR_IMAGE_INVALID_PARAMETER;
+    }
+    auto encodeFormat = iter->first;
+    SkBitmap bitmap;
+    TmpBufferHolder holder;
+    auto errorCode = BuildSkBitmap(pixelmap_, bitmap, encodeFormat, holder);
+    if (errorCode != SUCCESS) {
+        HiLog::Error(LABEL, "ExtEncoder::FinalizeEncode BuildSkBitmap failed");
+        return errorCode;
+    }
+    ExtWStream wStream(output_);
+    if (!SkEncodeImage(&wStream, bitmap, iter->first, opts_.quality)) {
+        HiLog::Error(LABEL, "ExtEncoder::FinalizeEncode encode failed");
+        return ERR_IMAGE_ENCODE_FAILED;
+    }
+    return SUCCESS;
+}
+} // namespace ImagePlugin
+} // namespace OHOS
---
+++ plugins/common/libs/image/libextplugin/src/ext_stream.cpp
@@ -0,0 +1,107 @@
+/*
+ * Copyright (C) 2023 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "ext_stream.h"
+#include "hilog/log.h"
+#include "log_tags.h"
+
+namespace {
+    constexpr OHOS::HiviewDFX::HiLogLabel LABEL = {LOG_CORE, LOG_TAG_DOMAIN_ID_PLUGIN, "ExtStream"};
+    constexpr static size_t SIZE_ZERO = 0;
+}
+namespace OHOS {
+namespace ImagePlugin {
+using namespace OHOS::HiviewDFX;
+struct InputStream {
+    uint8_t* buf;
+    uint32_t size;
+    uint32_t resSize;
+};
+
+ExtStream::ExtStream(InputDataStream *stream) : stream_(stream)
+{
+}
+
+static void InputStreamInit(InputStream &buf, uint8_t *input, size_t size)
+{
+    buf.buf = input;
+    buf.size = static_cast<uint32_t>(size);
+    buf.resSize = static_cast<uint32_t>(size);
+}
+
+static size_t Skip(InputDataStream *stream, size_t size)
+{
+    if (stream == nullptr) {
+        return SIZE_ZERO;
+    }
+    uint32_t cur = stream->Tell();
+    uint32_t seek = cur + static_cast<uint32_t>(size);
+    if (!stream->Seek(seek)) {
+        HiLog::Error(LABEL, "skip failed, curpositon=%{public}u, skip size=%{public}u", cur, size);
+        return SIZE_ZERO;
+    }
+    return size;
+}
+
+size_t ExtStream::read(void *buffer, size_t size)
+{
+    if (stream_ == nullptr) {
+        return SIZE_ZERO;
+    }
+    if (buffer == nullptr) {
+        return Skip(stream_, size);
+    }
+    InputStream buf;
+    InputStreamInit(buf, static_cast<uint8_t *>(buffer), size);
+    
+    uint32_t desiredSize = buf.size;
+    if (stream_->GetStreamSize() != SIZE_ZERO && stream_->GetStreamSize() < desiredSize) {
+        desiredSize = stream_->GetStreamSize();
+    }
+    if (!stream_->Read(desiredSize, buf.buf, buf.size, buf.resSize)) {
+        HiLog::Error(LABEL, "read failed, desire read size=%{public}u", buf.resSize);
+        return 0;
+    }
+    return static_cast<size_t>(buf.resSize);
+}
+
+size_t ExtStream::peek(void *buffer, size_t size) const
+{
+    if (stream_ == nullptr) {
+        return SIZE_ZERO;
+    }
+    if (buffer == nullptr) {
+        HiLog::Error(LABEL, "peek failed, output buffer is null");
+        return SIZE_ZERO;
+    }
+    InputStream buf;
+    InputStreamInit(buf, static_cast<uint8_t *>(buffer), size);
+    if (!stream_->Peek(buf.size, buf.buf, buf.size, buf.resSize)) {
+        HiLog::Error(LABEL, "peek failed, desire read size=%{public}u", buf.resSize);
+        return 0;
+    }
+    return static_cast<size_t>(buf.resSize);
+}
+
+bool ExtStream::isAtEnd() const
+{
+    if (stream_ == nullptr) {
+        return true;
+    }
+    size_t size = stream_->GetStreamSize();
+    return (stream_->Tell() == size);
+}
+} // namespace ImagePlugin
+} // namespace OHOS
