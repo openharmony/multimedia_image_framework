87a90d05599af351a6f9b32a4edc3d184cf916a6
yaozhupeng <yaozhupeng@huawei.com>
2023-09-27 20:27:04 +0800

    add freePixelmap mutex
    Signed-off-by: yaozhupeng <yaozhupeng@huawei.com>

--- frameworks/innerkitsimpl/common/src/pixel_map.cpp
+++ frameworks/innerkitsimpl/common/src/pixel_map.cpp
@@ -87,6 +87,7 @@ PixelMap::~PixelMap()
 
 void PixelMap::FreePixelMap() __attribute__((no_sanitize("cfi")))
 {
+    std::unique_lock<std::mutex> lock(*releaseMutex_);
     if (data_ == nullptr) {
         return;
     }
