cb3934db5867b4e6f83391fb1ba99438681c478b
杜高扬 <dugaoyang1@h-partners.com>
2023-11-27 09:10:12 +0000

    update frameworks/innerkitsimpl/test/unittest/image_source_test.cpp.
    Signed-off-by: 杜高扬 <dugaoyang1@h-partners.com>

--- frameworks/innerkitsimpl/test/unittest/image_source_test.cpp
+++ frameworks/innerkitsimpl/test/unittest/image_source_test.cpp
@@ -12,7 +12,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
+#define private public
 #include <algorithm>
 #include <fcntl.h>
 #include <fstream>
