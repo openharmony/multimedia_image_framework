b1c365d87a4ba08658d9629562476666631a388b
小蚊崽 <wangpiao3@h-partners.com>
2023-12-02 07:04:39 +0000

    update frameworks/innerkitsimpl/test/unittest/webp_encoder_test.cpp.
    Signed-off-by: 小蚊崽 <wangpiao3@h-partners.com>

--- frameworks/innerkitsimpl/test/unittest/webp_encoder_test.cpp
+++ frameworks/innerkitsimpl/test/unittest/webp_encoder_test.cpp
@@ -12,6 +12,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+#define private public
 #include <gtest/gtest.h>
 #include "webp_encoder.h"
 #include "image_source.h"
