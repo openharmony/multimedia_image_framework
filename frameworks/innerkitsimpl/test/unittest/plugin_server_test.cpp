28f196e6527a97d0a5b740abf622a4d8fb7ef82a
小蚊崽 <wangpiao3@h-partners.com>
2023-12-02 07:02:57 +0000

    update frameworks/innerkitsimpl/test/unittest/plugin_server_test.cpp.
    Signed-off-by: 小蚊崽 <wangpiao3@h-partners.com>

--- frameworks/innerkitsimpl/test/unittest/plugin_server_test.cpp
+++ frameworks/innerkitsimpl/test/unittest/plugin_server_test.cpp
@@ -12,6 +12,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+#define private public
 #include <gtest/gtest.h>
 #include "abs_image_detector.h"
 #include "image_source.h"
