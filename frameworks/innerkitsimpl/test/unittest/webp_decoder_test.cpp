de052ec0047a265f1b3b27fa7aadd0f0766784a5
小蚊崽 <wangpiao3@h-partners.com>
2023-12-02 12:31:37 +0000

    update frameworks/innerkitsimpl/test/unittest/webp_decoder_test.cpp.
    Signed-off-by: 小蚊崽 <wangpiao3@h-partners.com>

--- frameworks/innerkitsimpl/test/unittest/webp_decoder_test.cpp
+++ frameworks/innerkitsimpl/test/unittest/webp_decoder_test.cpp
@@ -12,7 +12,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-#define private public
 #include <gtest/gtest.h>
 #include "webp_decoder.h"
 #include "image_packer.h"
