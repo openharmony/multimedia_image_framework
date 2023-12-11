580ec74139615f4e27b5e6eb5c491726f7ba097f
小蚊崽 <wangpiao3@h-partners.com>
2023-12-11 06:07:37 +0000

    update frameworks/innerkitsimpl/test/unittest/jpeg_utils_test.cpp.
    Signed-off-by: 小蚊崽 <wangpiao3@h-partners.com>

--- frameworks/innerkitsimpl/test/unittest/jpeg_utils_test.cpp
+++ frameworks/innerkitsimpl/test/unittest/jpeg_utils_test.cpp
@@ -29,6 +29,85 @@ public:
     ~JpegUtilsTest() {}
 };
 
+/**
+ * @tc.name: FillInputBufferTest001
+ * @tc.desc: Test of FillInputBuffer
+ * @tc.type: FUNC
+ */
+HWTEST_F(JpegUtilsTest, FillInputBufferTest001, TestSize.Level3)
+{
+    GTEST_LOG_(INFO) << "JpegUtilsTest: FillInputBufferTest001 start";
+    j_decompress_ptr dinfo = nullptr;
+    boolean ret = ImagePlugin::FillInputBuffer(dinfo);
+    EXPECT_EQ(ret, FALSE);
+    GTEST_LOG_(INFO) << "JpegUtilsTest: FillInputBufferTest001 end";
+}
 
+/**
+ * @tc.name: SkipInputDataTest001
+ * @tc.desc: Test of SkipInputData
+ * @tc.type: FUNC
+ */
+HWTEST_F(JpegUtilsTest, SkipInputDataTest001, TestSize.Level3)
+{
+    GTEST_LOG_(INFO) << "JpegUtilsTest: SkipInputDataTest001 start";
+    j_decompress_ptr dinfo = nullptr;
+    long numBytes = 0;
+    ImagePlugin::SkipInputData(dinfo, numBytes);
+    GTEST_LOG_(INFO) << "JpegUtilsTest: SkipInputDataTest001 end";
+}
+
+/**
+ * @tc.name: TermSrcStreamTest001
+ * @tc.desc: Test of TermSrcStream
+ * @tc.type: FUNC
+ */
+HWTEST_F(JpegUtilsTest, TermSrcStreamTest001, TestSize.Level3)
+{
+    GTEST_LOG_(INFO) << "JpegUtilsTest: TermSrcStreamTest001 start";
+    j_decompress_ptr dinfo = nullptr;
+    ImagePlugin::TermSrcStream(dinfo);
+    GTEST_LOG_(INFO) << "JpegUtilsTest: TermSrcStreamTest001 end";
+}
+
+/**
+ * @tc.name: InitDstStreamTest001
+ * @tc.desc: Test of InitDstStream
+ * @tc.type: FUNC
+ */
+HWTEST_F(JpegUtilsTest, InitDstStreamTest001, TestSize.Level3)
+{
+    GTEST_LOG_(INFO) << "JpegUtilsTest: InitDstStreamTest001 start";
+    j_compress_ptr cinfo = nullptr;
+    ImagePlugin::InitDstStream(cinfo);
+    GTEST_LOG_(INFO) << "JpegUtilsTest: InitDstStreamTest001 end";
+}
+
+/**
+ * @tc.name: EmptyOutputBufferTest001
+ * @tc.desc: Test of EmptyOutputBuffer
+ * @tc.type: FUNC
+ */
+HWTEST_F(JpegUtilsTest, EmptyOutputBufferTest001, TestSize.Level3)
+{
+    GTEST_LOG_(INFO) << "JpegUtilsTest: EmptyOutputBufferTest001 start";
+    j_compress_ptr cinfo = nullptr;
+    boolean ret = ImagePlugin::EmptyOutputBuffer(cinfo);
+    ASSERT_EQ(ret, FALSE);
+    GTEST_LOG_(INFO) << "JpegUtilsTest: EmptyOutputBufferTest001 end";
+}
+
+/**
+ * @tc.name: TermDstStreamTest001
+ * @tc.desc: Test of TermDstStream
+ * @tc.type: FUNC
+ */
+HWTEST_F(JpegUtilsTest, TermDstStreamTest001, TestSize.Level3)
+{
+    GTEST_LOG_(INFO) << "JpegUtilsTest: TermDstStreamTest001 start";
+    j_compress_ptr cinfo = nullptr;
+    ImagePlugin::TermDstStream(cinfo);
+    GTEST_LOG_(INFO) << "JpegUtilsTest: TermDstStreamTest001 end";
+}
 }
 }
