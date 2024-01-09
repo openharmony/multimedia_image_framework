709e3416ca474e1cddae1ef3d209b7301aa470e0
沈治川 <shenzhichuan@h-partners.com>
2024-01-09 08:59:22 +0000

    update frameworks/innerkitsimpl/test/unittest/format_agent_plugin_src_test.cpp.
    Signed-off-by: 沈治川 <shenzhichuan@h-partners.com>

--- frameworks/innerkitsimpl/test/unittest/format_agent_plugin_src_test.cpp
+++ frameworks/innerkitsimpl/test/unittest/format_agent_plugin_src_test.cpp
@@ -12,7 +12,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
+#define private public
 #include <gtest/gtest.h>
 #include <fstream>
 #include "bmp_format_agent.h"
