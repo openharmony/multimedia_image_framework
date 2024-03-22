d533fea1dfe0f6f6d016dd5d8a98758e862a7c55
yanjunliang <yanjunliang@h-partners.com>
2024-03-22 16:47:37 +0800

    arkui-x update oh to 0318
    Signed-off-by: yanjunliang <yanjunliang@h-partners.com>
    Change-Id: If7821b1bafeb388d421baf1b762eb6a6c0df639b

--- frameworks/kits/js/common/image_source_napi.cpp
+++ frameworks/kits/js/common/image_source_napi.cpp
@@ -21,9 +21,7 @@
 #include "string_ex.h"
 #include "image_trace.h"
 #include "hitrace_meter.h"
-#if !defined(IOS_PLATFORM) && !defined(A_PLATFORM)
 #include "color_space_object_convertor.h"
-#endif
 
 #undef LOG_DOMAIN
 #define LOG_DOMAIN LOG_TAG_DOMAIN_ID_IMAGE
