b081c84eccd364bbaf1afa5b238918aab6347e2c
wangpiao <wangpiao3@h-partners.com>
2024-05-13 02:46:59 +0000

    黄区crash修复
    Signed-off-by: wangpiao <wangpiao3@h-partners.com>

--- frameworks/innerkitsimpl/test/unittest/scan_line_filter_test.cpp
+++ frameworks/innerkitsimpl/test/unittest/scan_line_filter_test.cpp
@@ -143,14 +143,14 @@ HWTEST_F(ScanLineFilterTest, FilterLineTest002, TestSize.Level3)
     GTEST_LOG_(INFO) << "ScanLineFilterTest: FilterLineTest002 start";
     ScanlineFilter scanlineFilter;
     void *destRowPixels = nullptr;
-    uint32_t destRowBytes = 256;
+    uint32_t destRowBytes = 0;
     void *srcRowPixels = nullptr;
     uint32_t ret = scanlineFilter.FilterLine(destRowPixels, destRowBytes, srcRowPixels);
     ASSERT_EQ(ret, ERR_IMAGE_CROP);
     destRowPixels = new uint8_t;
     srcRowPixels = new uint8_t;
     scanlineFilter.srcRegion_.width = 1;
-    scanlineFilter.srcBpp_ = -1;
+    scanlineFilter.srcBpp_ = 1;
     ret = scanlineFilter.FilterLine(destRowPixels, destRowBytes, srcRowPixels);
     ASSERT_EQ(ret, ERR_IMAGE_CROP);
     scanlineFilter.needPixelConvert_ = true;
