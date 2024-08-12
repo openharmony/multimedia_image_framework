0ab04d685b74f6366265e2d233d400cccf41787c
zhuzhihui7 <zhuzhihui7@huawei.com>
2024-08-12 19:14:07 +0800

    marshalling with yuvinfo
    Signed-off-by: zhuzhihui7 <zhuzhihui7@huawei.com>
    Change-Id: I178b8549c04675746a697534c4c3ecf620b3a285

--- frameworks/innerkitsimpl/common/src/pixel_map.cpp
+++ frameworks/innerkitsimpl/common/src/pixel_map.cpp
@@ -2083,6 +2083,55 @@ bool PixelMap::WriteTransformDataToParcel(Parcel &parcel) const
     return true;
 }
 
+bool PixelMap::WriteYuvDataInfoToParcel(Parcel &parcel) const
+{
+    if (IsYuvFormat()) {
+        if (!parcel.WriteInt32(static_cast<int32_t>(yuvDataInfo_.imageSize.width))) {
+            return false;
+        }
+        if (!parcel.WriteInt32(static_cast<int32_t>(yuvDataInfo_.imageSize.height))) {
+            return false;
+        }
+        if (!parcel.WriteInt32(static_cast<int32_t>(yuvDataInfo_.yWidth))) {
+            return false;
+        }
+        if (!parcel.WriteInt32(static_cast<int32_t>(yuvDataInfo_.yHeight))) {
+            return false;
+        }
+        if (!parcel.WriteInt32(static_cast<int32_t>(yuvDataInfo_.uvWidth))) {
+            return false;
+        }
+        if (!parcel.WriteInt32(static_cast<int32_t>(yuvDataInfo_.uvHeight))) {
+            return false;
+        }
+        if (!parcel.WriteInt32(static_cast<int32_t>(yuvDataInfo_.yStride))) {
+            return false;
+        }
+        if (!parcel.WriteInt32(static_cast<int32_t>(yuvDataInfo_.uStride))) {
+            return false;
+        }
+        if (!parcel.WriteInt32(static_cast<int32_t>(yuvDataInfo_.vStride))) {
+            return false;
+        }
+        if (!parcel.WriteInt32(static_cast<int32_t>(yuvDataInfo_.uvStride))) {
+            return false;
+        }
+        if (!parcel.WriteInt32(static_cast<int32_t>(yuvDataInfo_.yOffset))) {
+            return false;
+        }
+        if (!parcel.WriteInt32(static_cast<int32_t>(yuvDataInfo_.uOffset))) {
+            return false;
+        }
+        if (!parcel.WriteInt32(static_cast<int32_t>(yuvDataInfo_.vOffset))) {
+            return false;
+        }
+        if (!parcel.WriteInt32(static_cast<int32_t>(yuvDataInfo_.uvOffset))) {
+            return false;
+        }
+    }
+    return true;
+}
+
 bool PixelMap::WriteAstcRealSizeToParcel(Parcel &parcel) const
 {
     if (isAstc_) {
@@ -2128,6 +2177,11 @@ bool PixelMap::Marshalling(Parcel &parcel) const
         IMAGE_LOGE("write astcrealSize to parcel failed.");
         return false;
     }
+
+    if (!WriteYuvDataInfoToParcel(parcel)) {
+        IMAGE_LOGE("write WriteYuvDataInfoToParcel to parcel failed.");
+        return false;
+    }
     return true;
 }
 
@@ -2167,6 +2221,47 @@ bool PixelMap::ReadTransformData(Parcel &parcel, PixelMap *pixelMap)
     return true;
 }
 
+bool PixelMap::ReadYuvDataInfoFromParcel(Parcel &parcel, PixelMap *pixelMap)
+{
+    if (IsYuvFormat()) {
+        YUVDataInfo yDataInfo;
+        yDataInfo.imageSize.width = parcel.ReadInt32();
+        IMAGE_LOGD("ReadYuvDataInfoFromParcel width:%{public}d", yDataInfo.imageSize.width);
+        yDataInfo.imageSize.height = parcel.ReadInt32();
+        IMAGE_LOGD("ReadYuvDataInfoFromParcel height:%{public}d", yDataInfo.imageSize.height);
+
+        yDataInfo.yWidth = parcel.ReadInt32();
+        IMAGE_LOGD("ReadYuvDataInfoFromParcel yDataInfo.yWidth:%{public}d", yDataInfo.yWidth);
+        yDataInfo.yHeight = parcel.ReadInt32();
+        IMAGE_LOGD("ReadYuvDataInfoFromParcel yDataInfo.yHeight:%{public}d", yDataInfo.yHeight);
+        yDataInfo.uvWidth = parcel.ReadInt32();
+        IMAGE_LOGD("ReadYuvDataInfoFromParcel yDataInfo.uvWidth:%{public}d", yDataInfo.uvWidth);
+        yDataInfo.uvHeight = parcel.ReadInt32();
+        IMAGE_LOGD("ReadYuvDataInfoFromParcel yDataInfo.uvHeight:%{public}d", yDataInfo.uvHeight);
+
+        yDataInfo.yStride = parcel.ReadInt32();
+        IMAGE_LOGD("ReadYuvDataInfoFromParcel yDataInfo.yStride:%{public}d", yDataInfo.yStride);
+        yDataInfo.uStride = parcel.ReadInt32();
+        IMAGE_LOGD("ReadYuvDataInfoFromParcel yDataInfo.uStride:%{public}d", yDataInfo.uStride);
+        yDataInfo.vStride = parcel.ReadInt32();
+        IMAGE_LOGD("ReadYuvDataInfoFromParcel yDataInfo.vStride:%{public}d", yDataInfo.vStride);
+        yDataInfo.uvStride = parcel.ReadInt32();
+        IMAGE_LOGD("ReadYuvDataInfoFromParcel yDataInfo.uvStride:%{public}d", yDataInfo.uvStride);
+
+        yDataInfo.yOffset = parcel.ReadInt32();
+        IMAGE_LOGD("ReadYuvDataInfoFromParcel yDataInfo.yOffset:%{public}d", yDataInfo.yOffset);
+        yDataInfo.uOffset = parcel.ReadInt32();
+        IMAGE_LOGD("ReadYuvDataInfoFromParcel yDataInfo.uOffset:%{public}d", yDataInfo.uOffset);
+        yDataInfo.vOffset = parcel.ReadInt32();
+        IMAGE_LOGD("ReadYuvDataInfoFromParcel yDataInfo.vOffset:%{public}d", yDataInfo.vOffset);
+        yDataInfo.uvOffset = parcel.ReadInt32();
+        IMAGE_LOGD("ReadYuvDataInfoFromParcel yDataInfo.uvOffset:%{public}d", yDataInfo.uvOffset);
+
+        SetImageYUVInfo(yDataInfo);
+    }
+    return true;
+}
+
 bool PixelMap::ReadAstcRealSize(Parcel &parcel, PixelMap *pixelMap)
 {
     if (pixelMap->IsAstc()) {
@@ -2181,11 +2276,6 @@ bool PixelMap::ReadAstcRealSize(Parcel &parcel, PixelMap *pixelMap)
 bool PixelMap::ReadPropertiesFromParcel(Parcel &parcel, ImageInfo &imgInfo,
     AllocatorType &allocatorType, int32_t &bufferSize, PIXEL_MAP_ERR &error)
 {
-    if (!ReadImageInfo(parcel, imgInfo)) {
-        IMAGE_LOGE("read imageInfo fail");
-        PixelMap::ConstructPixelMapError(error, ERR_IMAGE_PIXELMAP_CREATE_FAILED, "read imageInfo fail");
-        return false;
-    }
     bool isEditable = parcel.ReadBool();
     SetEditable(isEditable);
     bool isAstc = parcel.ReadBool();
@@ -2301,13 +2391,31 @@ PixelMap *PixelMap::Unmarshalling(Parcel &parcel)
 
 PixelMap *PixelMap::Unmarshalling(Parcel &parcel, PIXEL_MAP_ERR &error)
 {
-    PixelMap *pixelMap = new(std::nothrow) PixelMap();
+    ImageInfo imgInfo;
+    PixelMap *pixelMap = nullptr;
+    if (!ReadImageInfo(parcel, imgInfo)) {
+        IMAGE_LOGE("read imageInfo fail");
+        PixelMap::ConstructPixelMapError(error, ERR_IMAGE_PIXELMAP_CREATE_FAILED, "read imageInfo fail");
+        return nullptr;
+    }
+    if (IsYUV(imgInfo.pixelFormat)) {
+#ifdef EXT_PIXEL
+        IMAGE_LOGE("creat PixelYuvExt.");
+        pixelMap = new(std::nothrow) PixelYuvExt();
+#else
+        IMAGE_LOGE("creat PixelYuv.");
+        pixelMap = new(std::nothrow) PixelYuv();
+#endif
+    } else {
+        IMAGE_LOGE("creat PixelMap.");
+        pixelMap = new(std::nothrow) PixelMap();
+    }
+
     if (pixelMap == nullptr) {
         PixelMap::ConstructPixelMapError(error, ERR_IMAGE_PIXELMAP_CREATE_FAILED, "pixelmap create failed");
         return nullptr;
     }
 
-    ImageInfo imgInfo;
     PixelMemInfo pixelMemInfo;
     if (!pixelMap->ReadPropertiesFromParcel(parcel, imgInfo, pixelMemInfo.allocatorType,
         pixelMemInfo.bufferSize, error)) {
@@ -2333,6 +2441,11 @@ PixelMap *PixelMap::Unmarshalling(Parcel &parcel, PIXEL_MAP_ERR &error)
     }
     if (!pixelMap->ReadAstcRealSize(parcel, pixelMap)) {
         IMAGE_LOGE("read astcrealSize fail");
+        delete pixelMap;
+        return nullptr;
+    }
+    if (!pixelMap->ReadYuvDataInfoFromParcel(parcel, pixelMap)) {
+        IMAGE_LOGE("ReadYuvDataInfoFromParcel fail");
         delete pixelMap;
         return nullptr;
     }
@@ -2552,7 +2665,7 @@ bool PixelMap::IsYuvFormat(PixelFormat format)
         format == PixelFormat::YCBCR_P010 || format == PixelFormat::YCRCB_P010;
 }
 
-bool PixelMap::IsYuvFormat()
+bool PixelMap::IsYuvFormat() const
 {
     return IsYuvFormat(imageInfo_.pixelFormat);
 }
@@ -2563,7 +2676,7 @@ void PixelMap::AssignYuvDataOnType(PixelFormat format, int32_t width, int32_t he
         yuvDataInfo_.yWidth = static_cast<uint32_t>(width);
         yuvDataInfo_.yHeight = static_cast<uint32_t>(height);
         yuvDataInfo_.yStride = static_cast<uint32_t>(width);
-        yuvDataInfo_.uvWidth = (width % NUM_2 == 0) ? static_cast<uint32_t>(width) : static_cast<uint32_t>(width + 1);
+        yuvDataInfo_.uvWidth = static_cast<uint32_t>((width + 1) / NUM_2);
         yuvDataInfo_.uvHeight = static_cast<uint32_t>((height + 1) / NUM_2);
         yuvDataInfo_.yOffset = 0;
         yuvDataInfo_.uvOffset =  yuvDataInfo_.yHeight * yuvDataInfo_.yStride;
@@ -2584,7 +2697,7 @@ void PixelMap::UpdateYUVDataInfo(PixelFormat format, int32_t width, int32_t heig
         yuvDataInfo_.yOffset = strides.yOffset;
         yuvDataInfo_.uvStride = strides.uvStride;
         yuvDataInfo_.uvOffset = strides.uvOffset;
-        yuvDataInfo_.uvWidth = static_cast<uint32_t>((width + 1) / NUM_2 * NUM_2);
+        yuvDataInfo_.uvWidth = static_cast<uint32_t>((width + 1) / NUM_2);
         yuvDataInfo_.uvHeight = static_cast<uint32_t>((height + 1) / NUM_2);
     }
 }
