2562c1219b8336ca0792316f3f93824e4317c105
何光耀 <heguangyao1@h-partners.com>
2024-08-23 18:09:10 +0800

    fix_codecheck_20240823 update001 Signed-off-by heguangyao <heguangyao1@h-partners.com>
    Signed-off-by: 何光耀 <heguangyao1@h-partners.com>

--- frameworks/kits/js/common/sendable/sendable_image_napi.cpp
+++ frameworks/kits/js/common/sendable/sendable_image_napi.cpp
@@ -684,6 +684,10 @@ static bool JsGetComponentArgs(napi_env env, size_t argc, napi_value* argv, Send
         const int32_t TEST_FORMAT = 12;
         format = TEST_FORMAT;
     } else {
+        if (native == nullptr) {
+        IMAGE_ERR("GetFormat: native is nullptr");
+        return false;
+    }
         native->GetFormat(format);
     }
 
