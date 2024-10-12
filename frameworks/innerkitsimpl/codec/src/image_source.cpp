0d81af7b23d87ea5b70a0ac9d41ce0b84f7bbdff
caochuan <caochuan@huawei.com>
2024-10-12 14:28:09 +0800

    Fix picture getAllProperties bug
    Signed-off-by: caochuan <caochuan@huawei.com>
    Change-Id: Id92a6f7be106e75e4a4aac1bad9c1e6c89ae636c

--- frameworks/innerkitsimpl/codec/src/image_source.cpp
+++ frameworks/innerkitsimpl/codec/src/image_source.cpp
@@ -4323,12 +4323,16 @@ std::unique_ptr<Picture> ImageSource::CreatePicture(const DecodingOptionsForPict
 
     std::set<AuxiliaryPictureType> auxTypes = (opts.desireAuxiliaryPictures.size() > 0) ?
             opts.desireAuxiliaryPictures : ImageUtils::GetAllAuxiliaryPictureType();
+    uint32_t auxErrorCode = SUCCESS;
     if (format == IMAGE_HEIF_FORMAT) {
-        DecodeHeifAuxiliaryPictures(auxTypes, picture, errorCode);
+        DecodeHeifAuxiliaryPictures(auxTypes, picture, auxErrorCode);
     } else if (format == IMAGE_JPEG_FORMAT) {
-        DecodeJpegAuxiliaryPicture(auxTypes, picture, errorCode);
+        DecodeJpegAuxiliaryPicture(auxTypes, picture, auxErrorCode);
     }
 
+    if (auxErrorCode != SUCCESS) {
+        IMAGE_LOGI("Decode auxiliary pictures failed, error code: %{public}u", auxErrorCode);
+    }
     return picture;
 }
 
@@ -4351,6 +4355,7 @@ void ImageSource::DecodeHeifAuxiliaryPictures(
             IMAGE_LOGE("Generate heif auxiliary picture failed! Type: %{public}d, errorCode: %{public}d",
                 auxType, errorCode);
         } else {
+            auxiliaryPicture->GetContentPixel()->SetEditable(true);
             picture->SetAuxiliaryPicture(auxiliaryPicture);
         }
     }
@@ -4391,6 +4396,7 @@ void ImageSource::DecodeJpegAuxiliaryPicture(
             if (auxPicture == nullptr) {
                 IMAGE_LOGE("Generate jepg auxiliary picture failed!, errorCode: %{public}d", errorCode);
             } else {
+                auxPicture->GetContentPixel()->SetEditable(true);
                 picture->SetAuxiliaryPicture(auxPicture);
             }
         }
