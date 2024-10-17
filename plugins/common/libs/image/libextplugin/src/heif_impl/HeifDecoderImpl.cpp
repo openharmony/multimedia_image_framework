090bd592cb34f07d632fb3cb6a0d5ff6e05a9ce9
chenzhaohui <chenzhaohui14@huawei.com>
2024-10-17 15:12:41 +0800

    Fixed HEIF format security vulnerability
    Signed-off-by: chenzhaohui <chenzhaohui14@huawei.com>
    Change-Id: I50501d581b12bd047cee27639bc831b24ed85b31

--- plugins/common/libs/image/libextplugin/src/heif_impl/HeifDecoderImpl.cpp
+++ plugins/common/libs/image/libextplugin/src/heif_impl/HeifDecoderImpl.cpp
@@ -584,11 +584,9 @@ bool HeifDecoderImpl::HwDecodeImage(HeifHardwareDecoder *hwDecoder,
         IMAGE_LOGE("unknown pixel type: %{public}d", outPixelFormat_);
         return false;
     }
-
     if (image == nullptr || outBuffer == nullptr) {
         return false;
     }
-
     bool isReuseHwDecoder = hwDecoder != nullptr;
     if (!isReuseHwDecoder) {
         hwDecoder = new (std::nothrow) HeifHardwareDecoder();
@@ -597,14 +595,12 @@ bool HeifDecoderImpl::HwDecodeImage(HeifHardwareDecoder *hwDecoder,
             return false;
         }
     }
-
     std::string imageType = parser_->GetItemType(image->GetItemId());
     if (imageType == "iden") {
         bool res = HwDecodeIdenImage(hwDecoder, image, gridInfo, outBuffer, isPrimary);
         ReleaseHwDecoder(hwDecoder, isReuseHwDecoder);
         return res;
     }
-
     GraphicPixelFormat inPixelFormat = GetInPixelFormat(image);
     sptr<SurfaceBuffer> hwBuffer = isPrimary && IsDirectYUVDecode() ? sptr<SurfaceBuffer>(dstHwBuffer_) :
                                    hwDecoder->AllocateOutputBuffer(gridInfo.tileWidth * gridInfo.cols,
