201e6ddc5b322b1439e1aaf0f09cf211ab019b7a
caochuan <caochuan@huawei.com>
2024-10-21 13:54:30 +0800

    Fix CreatePicture Napi OOM
    Issue: https://gitee.com/openharmony/multimedia_image_framework/issues/IAYISF
    
    Signed-off-by: caochuan <caochuan@huawei.com>
    Change-Id: Iaf2ae3435f8e7dfafbae0e446aced190fa06602a

--- interfaces/kits/js/common/include/picture_napi.h
+++ interfaces/kits/js/common/include/picture_napi.h
@@ -30,7 +30,7 @@ public:
 
     static napi_value Init(napi_env env, napi_value exports);
     static std::shared_ptr<Picture> GetPicture(napi_env env, napi_value picture);
-    static napi_value CreatePicture(napi_env env, std::shared_ptr<Picture> picture);
+    static napi_value CreatePicture(napi_env env, std::shared_ptr<Picture> &picture);
     static int32_t CreatePictureNapi(napi_env env, napi_value* result);
     void SetNativePicture(std::shared_ptr<Picture> picture);
     
