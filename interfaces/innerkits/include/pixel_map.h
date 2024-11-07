cf8e4c4b4e7797dbe70f2afe936db658f1e1eecf
陈乔异 <chenqiaoyi2@huawei.com>
2024-11-07 11:21:35 +0800

    删除重复函数
    Signed-off-by: 陈乔异 <chenqiaoyi2@huawei.com>

--- interfaces/innerkits/include/pixel_map.h
+++ interfaces/innerkits/include/pixel_map.h
@@ -375,10 +375,6 @@ public:
     static int32_t GetRGBxByteCount(const ImageInfo& info);
     static int32_t GetYUVByteCount(const ImageInfo& info);
     static int32_t GetAllocatedByteCount(const ImageInfo& info);
-    NATIVEEXPORT void SetAllocatorType(AllocatorType allocatorType)
-    {
-        allocatorType_ = allocatorType;
-    }
 
     NATIVEEXPORT uint32_t GetVersionId();
     NATIVEEXPORT void AddVersionId();
