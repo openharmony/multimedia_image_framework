aa09fdcc86d74128da6518cb6d243b2e74db47c1
liujinhong <liujinhong6@huawei.com>
2024-11-12 21:41:06 +0800

    heif 解码亮度差异
    Signed-off-by: liujinhong <liujinhong6@huawei.com>

--- plugins/common/libs/image/libextplugin/include/heif_impl/heif_parser/heif_error.h
+++ plugins/common/libs/image/libextplugin/include/heif_impl/heif_parser/heif_error.h
@@ -42,7 +42,6 @@ enum heif_error {
     heif_invalid_exif_data = 20,
     heif_invalid_mirror_direction = 21,
     heif_error_grid_too_large = 22,
-    heif_invalid_color_range_flag = 23,
 };
 } // namespace ImagePlugin
 } // namespace OHOS
