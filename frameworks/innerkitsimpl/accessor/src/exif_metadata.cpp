43ac47ee42c14ec5a0b5261962d67ba0d7e4eeab
caochuan <caochuan@huawei.com>
2024-11-12 17:08:08 +0800

    fix: Add limit of marshal
    Signed-off-by: caochuan <caochuan@huawei.com>
    Change-Id: I02c25377961bbf98bee0087adf80b03ede9045e8

--- frameworks/innerkitsimpl/accessor/src/exif_metadata.cpp
+++ frameworks/innerkitsimpl/accessor/src/exif_metadata.cpp
@@ -50,6 +50,7 @@ const auto TAG_VALUE_SIZE = 1024;
 const auto EXIF_HEAD_SIZE = 6;
 const static std::string DEFAULT_EXIF_VALUE = "default_exif_value";
 const static std::string HW_CAPTURE_MODE = "HwMnoteCaptureMode";
+const static uint64_t MAX_EXIFMETADATA_MAX_SIZE = 1024 * 1024;
 const std::set<std::string_view> HW_SPECIAL_KEYS = {
     "MovingPhotoId",
     "MovingPhotoVersion",
@@ -818,6 +819,11 @@ bool ExifMetadata::Marshalling(Parcel &parcel) const
         return false;
     }
 
+    if (size > MAX_EXIFMETADATA_MAX_SIZE) {
+        IMAGE_LOGE("The size of exif metadata exceeds the maximum limit.");
+        return false;
+    }
+
     if (data != nullptr && size != 0) {
         std::unique_ptr<unsigned char[]> exifData(data);
         if (!parcel.WriteUint32(static_cast<uint32_t>(size))) {
@@ -848,6 +854,11 @@ ExifMetadata *ExifMetadata::Unmarshalling(Parcel &parcel, PICTURE_ERR &error)
     if (hasExifDataBuffer) {
         uint32_t size = 0;
         if (!parcel.ReadUint32(size)) {
+            return nullptr;
+        }
+
+        if (size > MAX_EXIFMETADATA_MAX_SIZE) {
+            IMAGE_LOGE("The size of exif metadata exceeds the maximum limit.");
             return nullptr;
         }
         
