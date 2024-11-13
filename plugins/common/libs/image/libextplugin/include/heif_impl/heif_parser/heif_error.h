e7abfe5456caa22596de73b5dad7df28a2325db4
chenzhaohui <chenzhaohui14@huawei.com>
2024-11-13 13:01:00 +0800

    cherry pick 085a005 from https://gitee.com/chenzhaohui123456/multimedia_image_framework/pulls/2878 Fix Heif too many itemcount
    Signed-off-by: chenzhaohui <chenzhaohui14@huawei.com>
    Change-Id: I7fa47b4d8dec729861a0c2980cabf040ad073091

--- plugins/common/libs/image/libextplugin/include/heif_impl/heif_parser/heif_error.h
+++ plugins/common/libs/image/libextplugin/include/heif_impl/heif_parser/heif_error.h
@@ -42,6 +42,7 @@ enum heif_error {
     heif_invalid_exif_data = 20,
     heif_invalid_mirror_direction = 21,
     heif_error_grid_too_large = 22,
+    heif_error_too_many_item = 23,
 };
 } // namespace ImagePlugin
 } // namespace OHOS
