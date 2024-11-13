0398f5146358f1d7fea1d146f3ff1a04aadc778e
duanhan <duanhan1@huawei.com>
2024-11-13 17:52:29 +0800

    1113 getFilterArea errorCode fix Signed-off-by: duanhan <duanhan1@huawei.com>

--- frameworks/innerkitsimpl/codec/src/image_source.cpp
+++ frameworks/innerkitsimpl/codec/src/image_source.cpp
@@ -2578,7 +2578,7 @@ uint32_t ImageSource::GetFilterArea(const std::vector<std::string> &exifKeys,
         auto metadataAccessor = MetadataAccessorFactory::Create(bufferPtr, bufferSize);
         if (metadataAccessor == nullptr) {
             IMAGE_LOGD("Create metadataAccessor failed.");
-            return ERR_IMAGE_SOURCE_DATA;
+            return E_NO_EXIF_TAG;
         }
         return metadataAccessor->GetFilterArea(exifKeys, ranges);
     }
@@ -2592,7 +2592,7 @@ uint32_t ImageSource::GetFilterArea(const std::vector<std::string> &exifKeys,
     if (metadataAccessor == nullptr) {
         IMAGE_LOGD("Create metadataAccessor failed.");
         delete[] tmpBuffer;
-        return ERR_IMAGE_SOURCE_DATA;
+        return E_NO_EXIF_TAG;
     }
     auto ret = metadataAccessor->GetFilterArea(exifKeys, ranges);
     delete[] tmpBuffer;
