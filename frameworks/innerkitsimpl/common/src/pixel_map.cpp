36a6df74d477938ed49b8cf7140af3a434aa168b
陈乔异 <chenqiaoyi2@huawei.com>
2024-12-26 15:34:04 +0800

    优化释放逻辑
    Signed-off-by: 陈乔异 <chenqiaoyi2@huawei.com>

--- frameworks/innerkitsimpl/common/src/pixel_map.cpp
+++ frameworks/innerkitsimpl/common/src/pixel_map.cpp
@@ -3561,7 +3561,6 @@ static bool ShrinkRGBXToRGB(const std::unique_ptr<AbsMemory>& srcMemory, std::un
         dstMemory->Release();
         return false;
     }
-    srcMemory->Release();
     return true;
 }
 
@@ -3648,8 +3647,10 @@ bool PixelMap::DoTranslation(TransInfos &infos, const AntiAliasingOption &option
     if (imageInfo.pixelFormat == PixelFormat::RGB_888) {
         std::unique_ptr<AbsMemory> shrinkedMemory = nullptr;
         if (!ShrinkRGBXToRGB(dstMemory.memory, shrinkedMemory)) {
+            dstMemory->Release();
             return false;
         }
+        dstMemory->Release();
         m = shrinkedMemory.release();
     }
 
@@ -3831,8 +3832,10 @@ uint32_t PixelMap::crop(const Rect &rect)
     if (imageInfo.pixelFormat == PixelFormat::RGB_888) {
         std::unique_ptr<AbsMemory> shrinkedMemory = nullptr;
         if (!ShrinkRGBXToRGB(dstMemory.memory, shrinkedMemory)) {
+            dstMemory->Release();
             return false;
         }
+        dstMemory->Release();
         m = shrinkedMemory.release();
     }
 
