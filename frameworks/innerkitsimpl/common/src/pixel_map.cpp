625afb2648d71dcf86995a286422a0de286c1221
陈乔异 <chenqiaoyi2@huawei.com>
2024-12-26 15:21:47 +0800

    修改实现方式
    Signed-off-by: 陈乔异 <chenqiaoyi2@huawei.com>

--- frameworks/innerkitsimpl/common/src/pixel_map.cpp
+++ frameworks/innerkitsimpl/common/src/pixel_map.cpp
@@ -3743,6 +3743,51 @@ SkSamplingOptions ToSkSamplingOption(const AntiAliasingOption &option)
     }
 }
 
+static bool ExpandRGBToRGBX(const uint8_t* srcPixels, int32_t srcBytes, std::unique_ptr<uint8_t[]>& dstPixels)
+{
+    if (srcPixels == nullptr) {
+        IMAGE_LOGE("[PixelMap] ExpandRGBToRGBX failed: srcPixels is null");
+        return false;
+    }
+    int64_t dstBytes = srcBytes / RGB_888_BYTES * ARGB_8888_BYTES;
+    if (srcBytes <= 0 || dstBytes > INT32_MAX) {
+        IMAGE_LOGE("[PixelMap] ExpandRGBToRGBX failed: byte count invalid or overflowed");
+        return false;
+    }
+
+    dstPixels = std::make_unique<uint8_t[]>(dstBytes);
+    if (!PixelConvertAdapter::RGBToRGBx(srcPixels, dstPixels.get(), srcBytes)) {
+        IMAGE_LOGE("[PixelMap] ExpandRGBToRGBX failed: format conversion failed");
+        return false;
+    }
+    return true;
+}
+
+static bool ShrinkRGBXToRGB(const std::unique_ptr<AbsMemory>& srcMemory, std::unique_ptr<AbsMemory>& dstMemory)
+{
+    size_t srcBytes = srcMemory->data.size;
+    if (srcBytes > INT32_MAX) {
+        IMAGE_LOGE("[PixelMap] ShrinkRGBXToRGB failed: byte count too large");
+        return false;
+    }
+    int32_t dstBytes = static_cast<int32_t>(srcBytes) / ARGB_8888_BYTES * RGB_888_BYTES;
+    MemoryData memoryData = {nullptr, dstBytes, "Shrink RGBX to RGB"};
+    memoryData.format = PixelFormat::RGB_888;
+    dstMemory = MemoryManager::CreateMemory(srcMemory->GetType(), memoryData);
+    if (dstMemory == nullptr) {
+        IMAGE_LOGE("[PixelMap] ShrinkRGBXToRGB failed: allocate memory failed");
+        return false;
+    }
+
+    if (!PixelConvertAdapter::RGBxToRGB(static_cast<uint8_t*>(srcMemory->data.data),
+        static_cast<uint8_t*>(dstMemory->data.data), srcBytes)) {
+        IMAGE_LOGE("[PixelMap] ShrinkRGBXToRGB failed: format conversion failed");
+        return false;
+    }
+    srcMemory->Release();
+    return true;
+}
+
 void DrawImage(bool rectStaysRect, const AntiAliasingOption &option, SkCanvas &canvas, sk_sp<SkImage> &skImage)
 {
     if (rectStaysRect) {
@@ -3770,19 +3815,14 @@ bool PixelMap::DoTranslation(TransInfos &infos, const AntiAliasingOption &option
     TransMemoryInfo dstMemory;
     // We don't know how custom alloc memory
     dstMemory.allocType = (allocatorType_ == AllocatorType::CUSTOM_ALLOC) ? AllocatorType::DEFAULT : allocatorType_;
-
     SkTransInfo src;
-    std::unique_ptr<uint8_t[]> rgbxPixels = nullptr;
+
     if (imageInfo.pixelFormat == PixelFormat::RGB_888) {
         // Need this conversion because Skia uses 32-byte RGBX instead of 24-byte RGB when processing translation
-        int32_t srcByteCount = GetByteCount();
-        int64_t dstByteCount = srcByteCount / RGB_888_BYTES * ARGB_8888_BYTES;
-        if (srcByteCount <= 0 || dstByteCount > INT32_MAX) {
-            IMAGE_LOGE("[PixelMap] DoTranslation failed: byte count invalid or overflowed");
+        std::unique_ptr<uint8_t[]> rgbxPixels = nullptr;
+        if (!ExpandRGBToRGBX(data_, GetByteCount(), rgbxPixels)) {
             return false;
         }
-        rgbxPixels = std::make_unique<uint8_t[]>(dstByteCount);
-        PixelConvertAdapter::RGBToRGBx(data_, rgbxPixels.get(), srcByteCount);
         GenSrcTransInfo(src, imageInfo, rgbxPixels.get(), ToSkColorSpace(this));
     } else {
 #if !defined(_WIN32) && !defined(_APPLE) && !defined(IOS_PLATFORM) && !defined(ANDROID_PLATFORM)
@@ -3830,16 +3870,12 @@ bool PixelMap::DoTranslation(TransInfos &infos, const AntiAliasingOption &option
 
     if (imageInfo.pixelFormat == PixelFormat::RGB_888) {
         std::unique_ptr<AbsMemory> shrinkedMemory = nullptr;
-        int32_t dstRowStride = 0;
-        int errCode = AllocPixelMapMemory(shrinkedMemory, dstRowStride, imageInfo, false);
-        if (errCode != IMAGE_RESULT_SUCCESS) {
+        if (!ShrinkRGBXToRGB(dstMemory.memory, shrinkedMemory)) {
             return false;
         }
         m = shrinkedMemory.release();
-        PixelConvertAdapter::RGBxToRGB(static_cast<uint8_t*>(dstMemory.memory->data.data),
-            static_cast<uint8_t*>(m->data.data), dstMemory.memory->data.size);
-        dstMemory.memory->Release();
     }
+
     SetPixelsAddr(m->data.data, m->extend.data, m->data.size, m->GetType(), nullptr);
     SetImageInfo(imageInfo, true);
     if (origPixelFormat == PixelFormat::RGB_888 && !ExpandRGBToRGBX(this, true)) {
@@ -3984,19 +4020,14 @@ uint32_t PixelMap::crop(const Rect &rect)
     }
     ImageInfo imageInfo;
     GetImageInfo(imageInfo);
-
     SkTransInfo src;
-    std::unique_ptr<uint8_t[]> rgbxPixels = nullptr;
+
     if (imageInfo.pixelFormat == PixelFormat::RGB_888) {
         // Need this conversion because Skia uses 32-byte RGBX instead of 24-byte RGB when processing translation
-        int32_t srcByteCount = GetByteCount();
-        int64_t dstByteCount = srcByteCount / RGB_888_BYTES * ARGB_8888_BYTES;
-        if (srcByteCount <= 0 || dstByteCount > INT32_MAX) {
-            IMAGE_LOGE("[PixelMap] DoTranslation failed: byte count invalid or overflowed");
+        std::unique_ptr<uint8_t[]> rgbxPixels = nullptr;
+        if (!ExpandRGBToRGBX(data_, GetByteCount(), rgbxPixels)) {
             return false;
         }
-        rgbxPixels = std::make_unique<uint8_t[]>(dstByteCount);
-        PixelConvertAdapter::RGBToRGBx(data_, rgbxPixels.get(), srcByteCount);
         GenSrcTransInfo(src, imageInfo, rgbxPixels.get(), ToSkColorSpace(this));
     } else {
 #if !defined(_WIN32) && !defined(_APPLE) && !defined(IOS_PLATFORM) && !defined(ANDROID_PLATFORM)
@@ -4024,40 +4055,35 @@ uint32_t PixelMap::crop(const Rect &rect)
     Size desiredSize = {dst.info.width(), dst.info.height()};
     MemoryData memoryData = {nullptr, dst.info.computeMinByteSize(), "Trans ImageData", desiredSize,
                              imageInfo.pixelFormat};
-    auto m = MemoryManager::CreateMemory(allocatorType_, memoryData);
-    if (m == nullptr || m->data.data == nullptr) {
+    auto dstMemory = MemoryManager::CreateMemory(allocatorType_, memoryData);
+    if (dstMemory == nullptr || dstMemory->data.data == nullptr) {
         return ERR_IMAGE_CROP;
     }
     uint64_t rowStride = dst.info.minRowBytes();
 #if !defined(IOS_PLATFORM) && !defined(ANDROID_PLATFORM)
     if (allocatorType_ == AllocatorType::DMA_ALLOC) {
-        if (m->extend.data == nullptr) {
+        if (dstMemory->extend.data == nullptr) {
             IMAGE_LOGE("GendstTransInfo get surfacebuffer failed");
             return ERR_IMAGE_CROP;
         }
-        rowStride = static_cast<uint64_t>(static_cast<SurfaceBuffer*>(m->extend.data)->GetStride());
+        rowStride = static_cast<uint64_t>(static_cast<SurfaceBuffer*>(dstMemory->extend.data)->GetStride());
     }
 #endif
-    if (!src.bitmap.readPixels(dst.info, m->data.data, rowStride, dstIRect.fLeft, dstIRect.fTop)) {
-        m->Release();
+    if (!src.bitmap.readPixels(dst.info, dstMemory->data.data, rowStride, dstIRect.fLeft, dstIRect.fTop)) {
+        dstMemory->Release();
         IMAGE_LOGE("ReadPixels failed");
         return ERR_IMAGE_CROP;
     }
     ToImageInfo(imageInfo, dst.info);
-    CopySurfaceBufferInfo(m->extend.data);
+    CopySurfaceBufferInfo(dstMemory->extend.data);
 
+    auto m = dstMemory.get();
     if (imageInfo.pixelFormat == PixelFormat::RGB_888) {
         std::unique_ptr<AbsMemory> shrinkedMemory = nullptr;
-        int32_t dstRowStride = 0;
-        int errCode = AllocPixelMapMemory(shrinkedMemory, dstRowStride, imageInfo, false);
-        if (errCode != IMAGE_RESULT_SUCCESS) {
+        if (!ShrinkRGBXToRGB(dstMemory.memory, shrinkedMemory)) {
             return false;
         }
-        auto prev = m;
         m = shrinkedMemory.release();
-        PixelConvertAdapter::RGBxToRGB(static_cast<uint8_t*>(prev->data.data),
-            static_cast<uint8_t*>(m->data.data), prev->data.size);
-        m->Release();
     }
 
     SetPixelsAddr(m->data.data, m->extend.data, m->data.size, m->GetType(), nullptr);
