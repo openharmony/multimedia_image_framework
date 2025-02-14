95c61cd7e30770aec7728bf38c976c3716ce5d0f
chenzhaohui <chenzhaohui14@huawei.com>
2025-02-14 12:02:18 +0800

    fix abnormal branch memory not released
    Signed-off-by: chenzhaohui <chenzhaohui14@huawei.com>
    Change-Id: I57ed6ce9941e262361d3123545a6c23e0a6eaa0a

--- frameworks/kits/js/common/image_napi.cpp
+++ frameworks/kits/js/common/image_napi.cpp
@@ -604,6 +604,7 @@ static void TestGetComponentCallBack(napi_env env, napi_status status, ImageAsyn
     napi_value array;
     void *nativePtr = nullptr;
     if (napi_create_arraybuffer(env, NUM1, &nativePtr, &array) != napi_ok || nativePtr == nullptr) {
+        napi_get_undefined(env, &result);
         context->status = ERROR;
         CommonCallbackRoutine(env, context, result);
         return;
