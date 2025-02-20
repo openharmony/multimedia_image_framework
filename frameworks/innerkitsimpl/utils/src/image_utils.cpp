a0bf62ff11b0edfc6cc34221f98bc53467b87c23
liujinhong <liujinhong6@huawei.com>
2025-02-20 21:33:52 +0800

    utils安全整改
    Signed-off-by: liujinhong <liujinhong6@huawei.com>
    Change-Id: Iafc3a91d33023b156e31e210f6116a8499170e03

--- frameworks/innerkitsimpl/utils/src/image_utils.cpp
+++ frameworks/innerkitsimpl/utils/src/image_utils.cpp
@@ -657,10 +657,10 @@ std::string ImageUtils::GetPixelMapName(PixelMap* pixelMap)
 }
 
  // BytesToUint16 function will modify the offset value.
-uint16_t ImageUtils::BytesToUint16(uint8_t* bytes, uint32_t& offset, bool isBigEndian)
+uint16_t ImageUtils::BytesToUint16(uint8_t* bytes, uint32_t& offset, uint32_t size, bool isBigEndian)
 {
     uint16_t data = 0;
-    if (bytes == nullptr) {
+    if (bytes == nullptr || offset + NUM_2 >= size) {
         return data;
     }
     if (isBigEndian) {
@@ -673,10 +673,10 @@ uint16_t ImageUtils::BytesToUint16(uint8_t* bytes, uint32_t& offset, bool isBigE
 }
 
 // BytesToUint32 function will modify the offset value.
-uint32_t ImageUtils::BytesToUint32(uint8_t* bytes, uint32_t& offset, bool isBigEndian)
+uint32_t ImageUtils::BytesToUint32(uint8_t* bytes, uint32_t& offset, uint32_t size, bool isBigEndian)
 {
     uint32_t data = 0;
-    if (bytes == nullptr) {
+    if (bytes == nullptr || offset + NUM_4 >= size) {
         return data;
     }
     if (isBigEndian) {
@@ -691,10 +691,10 @@ uint32_t ImageUtils::BytesToUint32(uint8_t* bytes, uint32_t& offset, bool isBigE
 }
 
 // BytesToInt32 function will modify the offset value.
-int32_t ImageUtils::BytesToInt32(uint8_t* bytes, uint32_t& offset, bool isBigEndian)
+int32_t ImageUtils::BytesToInt32(uint8_t* bytes, uint32_t& offset, uint32_t size, bool isBigEndian)
 {
     int32_t data = 0;
-    if (bytes == nullptr) {
+    if (bytes == nullptr || offset + NUM_4 >= size) {
         return data;
     }
     if (isBigEndian) {
@@ -709,9 +709,9 @@ int32_t ImageUtils::BytesToInt32(uint8_t* bytes, uint32_t& offset, bool isBigEnd
 }
 
 // BytesToFloat function will modify the offset value.
-float ImageUtils::BytesToFloat(uint8_t* bytes, uint32_t& offset, bool isBigEndian)
+float ImageUtils::BytesToFloat(uint8_t* bytes, uint32_t& offset, uint32_t size, bool isBigEndian)
 {
-    uint32_t data = BytesToUint32(bytes, offset, isBigEndian);
+    uint32_t data = BytesToUint32(bytes, offset, size, isBigEndian);
     union {
         uint32_t i;
         float f;
