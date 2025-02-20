d9da09138579ee879025b10a206209d05123d623
renhw001 <renhongwei9@huawei.com>
2025-02-20 16:16:21 +0800

    code check
    Signed-off-by: renhw001 <renhongwei9@huawei.com>

--- frameworks/innerkitsimpl/test/unittest/pixel_map_test/image_pixel_map_test.cpp
+++ frameworks/innerkitsimpl/test/unittest/pixel_map_test/image_pixel_map_test.cpp
@@ -18,6 +18,7 @@
 #include "media_errors.h"
 #include "pixel_map.h"
 #include "color_space.h"
+#include "post_proc.h"
 
 using namespace testing::ext;
 using namespace OHOS::Media;
@@ -2233,6 +2234,56 @@ HWTEST_F(ImagePixelMapTest, ImagePixelMapCreate002, TestSize.Level3)
     ASSERT_EQ(ret, 0);
 
     GTEST_LOG_(INFO) << "ImagePixelMapTest: ImagePixelMapCreate002 scale end";
+}
+
+/**
+* @tc.name: ImagePixelMapSLRWithlap001
+* @tc.desc: test SLR with Laplacian
+* @tc.type: FUNC
+*/
+HWTEST_F(ImagePixelMapTest, ImagePixelMapSLRWithLap001, TestSize.Level3)
+{
+    GTEST_LOG_(INFO) << "ImagePixelMapTest: ImagePixelMapSLRWithLap001 scale start";
+    std::unique_ptr<PixelMap> pixelMap = ConstructSLRPixelMap();
+    EXPECT_NE(pixelMap, nullptr);
+    ImageInfo imageInfo;
+    pixelMap->GetImageInfo(imageInfo);
+
+    float xAxis = 0.7f; // 0.7f scale test
+    float yAxis = 0.9f; // 0.9f scale test
+    Size desiredSize;
+    desiredSize.width = static_cast<int32_t>(imageInfo.size.width * xAxis);
+    desiredSize.height = static_cast<int32_t>(imageInfo.size.height * yAxis);
+
+    PostProc postProc;
+    bool ret = postProc.ScalePixelMapWithSLR(desiredSize, *pixelMap.get(), true);
+    EXPECT_NE(ret, false);
+    GTEST_LOG_(INFO) << "ImagePixelMapTest: ImagePixelMapSLRWithLap001 scale end";
+}
+
+/**
+* @tc.name: ImagePixelMapSLRWithlap002
+* @tc.desc: test SLR with Laplacian
+* @tc.type: FUNC
+*/
+HWTEST_F(ImagePixelMapTest, ImagePixelMapSLRWithlap002, TestSize.Level3)
+{
+    GTEST_LOG_(INFO) << "ImagePixelMapTest: ImagePixelMapSLRWithlap002 scale start";
+    std::unique_ptr<PixelMap> pixelMap = ConstructSLRPixelMap();
+    EXPECT_NE(pixelMap, nullptr);
+    ImageInfo imageInfo;
+    pixelMap->GetImageInfo(imageInfo);
+
+    float xAxis = 0.7f; // 0.7f scale test
+    float yAxis = 0.9f; // 0.9f scale test
+    Size desiredSize;
+    desiredSize.width = static_cast<int32_t>(imageInfo.size.width * xAxis);
+    desiredSize.height = static_cast<int32_t>(imageInfo.size.height * yAxis);
+
+    PostProc postProc;
+    bool ret = postProc.ScalePixelMapWithSLR(desiredSize, *pixelMap.get(), false);
+    EXPECT_NE(ret, false);
+    GTEST_LOG_(INFO) << "ImagePixelMapTest: ImagePixelMapSLRWithlap002 scale end";
 }
 } // namespace Multimedia
 } // namespace OHOS
