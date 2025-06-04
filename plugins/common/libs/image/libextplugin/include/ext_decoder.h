9c5d2547f1f201ef7258d6eace78338bd63d636e
NyquistLi <lichengcheng30@huawei.com>
2025-06-04 19:24:00 +0800

    fix initJpegErr_
    Signed-off-by: NyquistLi <lichengcheng30@huawei.com>

--- plugins/common/libs/image/libextplugin/include/ext_decoder.h
+++ plugins/common/libs/image/libextplugin/include/ext_decoder.h
@@ -188,7 +188,7 @@ private:
     uint32_t gainMapOffset_ = 0;
 #ifdef JPEG_HW_DECODE_ENABLE
     std::shared_ptr<JpegHardwareDecoder> hwDecoderPtr_ = nullptr;
-    bool initJpegErr_ = flase;
+    bool initJpegErr_ = false;
 #endif
 };
 } // namespace ImagePlugin
