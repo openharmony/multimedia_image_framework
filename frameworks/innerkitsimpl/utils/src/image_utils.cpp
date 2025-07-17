6a415c638d665ee18c8529cb64cc73de231e1be0
fanzhihao8 <fanzhihao6@h-partners.com>
2025-07-17 14:34:00 +0800

    fix bug for codecheck
    Signed-off-by: fanzhihao8 <fanzhihao6@h-partners.com>

--- frameworks/innerkitsimpl/utils/src/image_utils.cpp
+++ frameworks/innerkitsimpl/utils/src/image_utils.cpp
@@ -806,7 +806,7 @@ std::string ImageUtils::GetLocalTime()
     auto ms = std::chrono::duration_cast<std::chrono::milliseconds>(now.time_since_epoch()) % 1000;
     std::time_t t = std::chrono::system_clock::to_time_t(now);
     std::tm tm = *std::localtime(&t);
-    if (!tm) {
+    if (tm == nullptr) {
         IMAGE_LOGE("ImageUtils::GetLocalTime error, returned nullptr");
         return "";
     }
