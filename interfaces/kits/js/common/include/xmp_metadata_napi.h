a0b12e44cfa2504c2a55a8758587c8779d644fa4
zhaona45 <zhaona45@huawei.com>
2025-11-25 14:33:43 +0800

    增加XMPMetadataNapi基础框架
    Signed-off-by: zhaona45 <zhaona45@huawei.com>
    Change-Id: Ia59f7056b7c59aaad9b39ba0dd55e37161469241

---
+++ interfaces/kits/js/common/include/xmp_metadata_napi.h
@@ -0,0 +1,47 @@
+/*
+ * Copyright (C) 2025 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef INTERFACES_KITS_JS_COMMON_INCLUDE_XMP_METADATA_NAPI_H
+#define INTERFACES_KITS_JS_COMMON_INCLUDE_XMP_METADATA_NAPI_H
+
+#include "napi/native_api.h"
+#include "napi/native_node_api.h"
+#include "xmp_metadata.h"
+
+namespace OHOS {
+namespace Media {
+class XMPMetadataNapi {
+public:
+    XMPMetadataNapi();
+    ~XMPMetadataNapi();
+
+    static napi_value Init(napi_env env, napi_value exports);
+
+private:
+    static napi_value Constructor(napi_env env, napi_callback_info info);
+    static void Destructor(napi_env env, void *nativeObject, void *finalize);
+
+    // void release();
+
+    static thread_local napi_ref sConstructor_;
+    static thread_local std::shared_ptr<XMPMetadata> sXMPMetadata_;
+    std::shared_ptr<XMPMetadata> nativeXMPMetadata_;
+    napi_env env_ = nullptr;
+    bool isRelease = false;
+    // uint32_t uniqueId_ = 0;
+};
+} // namespace Media
+} // namespace OHOS
+#endif // INTERFACES_KITS_JS_COMMON_INCLUDE_XMP_METADATA_NAPI_H
