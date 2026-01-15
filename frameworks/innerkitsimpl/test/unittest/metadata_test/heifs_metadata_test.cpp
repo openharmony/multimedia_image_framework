e0b5c469a083a866ba55a88196f9c021014b67f2
zhaona45 <zhaona45@huawei.com>
2026-01-15 11:18:20 +0800

    Fix KvMetadata CDE Check
    Signed-off-by: zhaona45 <zhaona45@huawei.com>

--- frameworks/innerkitsimpl/test/unittest/metadata_test/heifs_metadata_test.cpp
+++ frameworks/innerkitsimpl/test/unittest/metadata_test/heifs_metadata_test.cpp
@@ -19,6 +19,7 @@
 #include <iostream>
 
 #include "kv_metadata.h"
+#include "gif_metadata.h"
 #include "heifs_metadata.h"
 #include "media_errors.h"
 #include "image_type.h"
@@ -29,45 +30,7 @@ using namespace testing::ext;
 
 namespace {
     static const std::string MOCK_DELAY_TIME = "40";
-    static const uint32_t FIXED_BUFFER_SIZE = sizeof(uint32_t) + sizeof(uint64_t);
-    static const uint32_t MOCK_BUFFER_SIZE = FIXED_BUFFER_SIZE + sizeof(uint32_t) * 2 + 14 + 2;
-    static const uint32_t LARGE_BUFFER_SIZE = MOCK_BUFFER_SIZE * 2;
-    static const uint32_t MAX_KV_METADATA_COUNT = 10;
-    static const uint8_t MOCK_METADATA_COUNT = MAX_KV_METADATA_COUNT + 1;
-    static const uint8_t ALPHA_E = 0x65;
-#if __BYTE_ORDER == __LITTLE_ENDIAN
-    static const uint32_t METADATA_TYPE_INDEX = 0;
-    static const uint32_t METADATA_COUNT_INDEX = 4;
-    static const uint32_t METADATA_KEY_INDEX = 12;
-    static const uint8_t MOCK_METADATA[] = { 0x0F, 0x00, 0x00, 0x00,
-                                             0x01, 0x00, 0x00, 0x00,
-                                             0x00, 0x00, 0x00, 0x00,
-                                             0x0E, 0x00, 0x00, 0x00,
-                                             0x48, 0x65, 0x69, 0x66, 0x73,
-                                             0x44, 0x65, 0x6C, 0x61, 0x79,
-                                             0x54, 0x69, 0x6D, 0x65,
-                                             0x02, 0x00, 0x00, 0x00,
-                                             0x34, 0x30 };
-    static const uint8_t NULL_METADATA[] = { 0x0F, 0x00, 0x00, 0x00,
-                                             0x00, 0x00, 0x00, 0x00,
-                                             0x00, 0x00, 0x00, 0x00 };
-#else
-    static const uint32_t METADATA_TYPE_INDEX = 3;
-    static const uint32_t METADATA_COUNT_INDEX = 11;
-    static const uint32_t METADATA_KEY_INDEX = 15;
-    static const uint8_t MOCK_METADATA[] = { 0x00, 0x00, 0x00, 0x0F,
-                                             0x00, 0x00, 0x00, 0x00,
-                                             0x00, 0x00, 0x00, 0x01,
-                                             0x00, 0x00, 0x00, 0x0E,
-                                             0x48, 0x65, 0x69, 0x66, 0x73,
-                                             0x44, 0x65, 0x6C, 0x61, 0x79,
-                                             0x54, 0x69, 0x6D, 0x65,
-                                             0x00, 0x00, 0x00, 0x02,
-                                             0x34, 0x30 };
-    static const uint8_t NULL_METADATA[] = { 0x00, 0x00, 0x00, 0x0F,
-                                             0x00, 0x00, 0x00, 0x00,
-                                             0x00, 0x00, 0x00, 0x00 };
-#endif
+    static const uint32_t MOCK_BUFFER_SIZE = 40;
 }
 
 namespace OHOS {
@@ -89,7 +52,7 @@ static std::unique_ptr<HeifsMetadata> ConstructHeifsMetadata(bool isNull)
 
 /**
  * @tc.name: GetBlobTest001
- * @tc.desc: Test GetBlob when metadata has properties and dst buffer is too small.
+ * @tc.desc: Test GetBlob when metadata has properties and dst buffer is nullptr.
  * @tc.type: FUNC
  */
 HWTEST_F(HeifsMetadataTest, GetBlobTest001, TestSize.Level3)
@@ -97,101 +60,51 @@ HWTEST_F(HeifsMetadataTest, GetBlobTest001, TestSize.Level3)
     GTEST_LOG_(INFO) << "HeifsMetadataTest: GetBlobTest001 start";
     std::unique_ptr<HeifsMetadata> metadata = ConstructHeifsMetadata(false);
     ASSERT_NE(metadata, nullptr);
-    std::unique_ptr<uint8_t[]> dst = std::make_unique<uint8_t[]>(FIXED_BUFFER_SIZE);
-    ASSERT_NE(dst, nullptr);
-    ASSERT_EQ(metadata->GetBlob(FIXED_BUFFER_SIZE, dst.get()), ERR_IMAGE_INVALID_PARAMETER);
+    uint8_t *dst = nullptr;
+    ASSERT_EQ(metadata->GetBlob(0, dst), ERR_IMAGE_INVALID_PARAMETER);
     GTEST_LOG_(INFO) << "HeifsMetadataTest: GetBlobTest001 end";
 }
 
 /**
  * @tc.name: GetBlobTest002
- * @tc.desc: Test GetBlob when metadata has properties and dst buffer is sufficient.
+ * @tc.desc: Test GetBlob when metadata properties is nullptr.
  * @tc.type: FUNC
  */
 HWTEST_F(HeifsMetadataTest, GetBlobTest002, TestSize.Level3)
 {
     GTEST_LOG_(INFO) << "HeifsMetadataTest: GetBlobTest002 start";
-    std::unique_ptr<HeifsMetadata> metadata = ConstructHeifsMetadata(false);
+    std::unique_ptr<HeifsMetadata> metadata = ConstructHeifsMetadata(true);
     ASSERT_NE(metadata, nullptr);
+    metadata->properties_.reset();
     std::unique_ptr<uint8_t[]> dst = std::make_unique<uint8_t[]>(MOCK_BUFFER_SIZE);
     ASSERT_NE(dst, nullptr);
-    ASSERT_EQ(metadata->GetBlobSize(), MOCK_BUFFER_SIZE);
-    ASSERT_EQ(metadata->GetBlob(MOCK_BUFFER_SIZE, dst.get()), SUCCESS);
-    for (uint32_t i = 0; i < MOCK_BUFFER_SIZE; i++) {
-        ASSERT_EQ(dst[i], MOCK_METADATA[i]);
-    }
+    ASSERT_EQ(metadata->GetBlob(MOCK_BUFFER_SIZE, dst.get()), ERR_IMAGE_INVALID_PARAMETER);
     GTEST_LOG_(INFO) << "HeifsMetadataTest: GetBlobTest002 end";
 }
 
 /**
  * @tc.name: GetBlobTest003
- * @tc.desc: Test GetBlob when metadata has properties and dst buffer is large.
+ * @tc.desc: Test GetBlob when metadata has properties.
  * @tc.type: FUNC
  */
 HWTEST_F(HeifsMetadataTest, GetBlobTest003, TestSize.Level3)
 {
     GTEST_LOG_(INFO) << "HeifsMetadataTest: GetBlobTest003 start";
-    std::unique_ptr<HeifsMetadata> metadata = ConstructHeifsMetadata(false);
-    ASSERT_NE(metadata, nullptr);
-    std::unique_ptr<uint8_t[]> dst = std::make_unique<uint8_t[]>(LARGE_BUFFER_SIZE);
+    std::unique_ptr<HeifsMetadata> metadataSrc = ConstructHeifsMetadata(false);
+    ASSERT_NE(metadataSrc, nullptr);
+    std::unique_ptr<HeifsMetadata> metadataDst = ConstructHeifsMetadata(true);
+    uint32_t size = metadataSrc->GetBlobSize();
+    ASSERT_NE(size, 0);
+    std::unique_ptr<uint8_t[]> dst = std::make_unique<uint8_t[]>(size);
     ASSERT_NE(dst, nullptr);
-    ASSERT_EQ(metadata->GetBlob(LARGE_BUFFER_SIZE, dst.get()), SUCCESS);
-    for (uint32_t i = 0; i < MOCK_BUFFER_SIZE; i++) {
-        ASSERT_EQ(dst[i], MOCK_METADATA[i]);
-    }
+    ASSERT_EQ(metadataSrc->GetBlob(size, dst.get()), SUCCESS);
+    ASSERT_EQ(metadataDst->SetBlob(dst.get(), size), SUCCESS);
+    std::string delayTimeSrc;
+    ASSERT_EQ(metadataSrc->GetValue(HEIFS_METADATA_KEY_DELAY_TIME, delayTimeSrc), SUCCESS);
+    std::string delayTimeDst;
+    ASSERT_EQ(metadataDst->GetValue(HEIFS_METADATA_KEY_DELAY_TIME, delayTimeDst), SUCCESS);
+    ASSERT_EQ(delayTimeDst, delayTimeSrc);
     GTEST_LOG_(INFO) << "HeifsMetadataTest: GetBlobTest003 end";
-}
-
-/**
- * @tc.name: GetBlobTest004
- * @tc.desc: Test GetBlob when metadata has properties and dst buffer is nullptr.
- * @tc.type: FUNC
- */
-HWTEST_F(HeifsMetadataTest, GetBlobTest004, TestSize.Level3)
-{
-    GTEST_LOG_(INFO) << "HeifsMetadataTest: GetBlobTest004 start";
-    std::unique_ptr<HeifsMetadata> metadata = ConstructHeifsMetadata(true);
-    ASSERT_NE(metadata, nullptr);
-    uint8_t *dst = nullptr;
-    ASSERT_EQ(metadata->GetBlob(LARGE_BUFFER_SIZE, dst), ERR_IMAGE_INVALID_PARAMETER);
-    GTEST_LOG_(INFO) << "HeifsMetadataTest: GetBlobTest004 end";
-}
-
-/**
- * @tc.name: GetBlobTest005
- * @tc.desc: Test GetBlob when metadata doesn't have properties.
- * @tc.type: FUNC
- */
-HWTEST_F(HeifsMetadataTest, GetBlobTest005, TestSize.Level3)
-{
-    GTEST_LOG_(INFO) << "HeifsMetadataTest: GetBlobTest005 start";
-    std::unique_ptr<HeifsMetadata> metadata = ConstructHeifsMetadata(true);
-    ASSERT_NE(metadata, nullptr);
-    std::unique_ptr<uint8_t[]> dst = std::make_unique<uint8_t[]>(FIXED_BUFFER_SIZE);
-    ASSERT_NE(dst, nullptr);
-    ASSERT_EQ(metadata->GetBlobSize(), FIXED_BUFFER_SIZE);
-    ASSERT_EQ(metadata->GetBlob(FIXED_BUFFER_SIZE, dst.get()), SUCCESS);
-    for (uint32_t i = 0; i < FIXED_BUFFER_SIZE; i++) {
-        ASSERT_EQ(dst[i], NULL_METADATA[i]);
-    }
-    GTEST_LOG_(INFO) << "HeifsMetadataTest: GetBlobTest005 end";
-}
-
-/**
- * @tc.name: GetBlobTest006
- * @tc.desc: Test GetBlob when metadata properties is nullptr.
- * @tc.type: FUNC
- */
-HWTEST_F(HeifsMetadataTest, GetBlobTest006, TestSize.Level3)
-{
-    GTEST_LOG_(INFO) << "HeifsMetadataTest: GetBlobTest006 start";
-    std::unique_ptr<HeifsMetadata> metadata = ConstructHeifsMetadata(true);
-    ASSERT_NE(metadata, nullptr);
-    metadata->properties_.reset();
-    std::unique_ptr<uint8_t[]> dst = std::make_unique<uint8_t[]>(FIXED_BUFFER_SIZE);
-    ASSERT_NE(dst, nullptr);
-    ASSERT_EQ(metadata->GetBlob(FIXED_BUFFER_SIZE, dst.get()), ERR_IMAGE_INVALID_PARAMETER);
-    GTEST_LOG_(INFO) << "HeifsMetadataTest: GetBlobTest006 end";
 }
 
 /**
@@ -205,7 +118,9 @@ HWTEST_F(HeifsMetadataTest, SetBlobTest001, TestSize.Level3)
     std::unique_ptr<HeifsMetadata> metadata = ConstructHeifsMetadata(true);
     ASSERT_NE(metadata, nullptr);
     metadata->properties_.reset();
-    ASSERT_EQ(metadata->SetBlob(MOCK_METADATA, MOCK_BUFFER_SIZE), SUCCESS);
+    std::unique_ptr<uint8_t[]> src = std::make_unique<uint8_t[]>(MOCK_BUFFER_SIZE);
+    ASSERT_NE(src, nullptr);
+    ASSERT_EQ(metadata->SetBlob(src.get(), MOCK_BUFFER_SIZE), ERR_IMAGE_INVALID_PARAMETER);
     GTEST_LOG_(INFO) << "HeifsMetadataTest: SetBlobTest001 end";
 }
 
@@ -231,92 +146,39 @@ HWTEST_F(HeifsMetadataTest, SetBlobTest002, TestSize.Level3)
 HWTEST_F(HeifsMetadataTest, SetBlobTest003, TestSize.Level3)
 {
     GTEST_LOG_(INFO) << "HeifsMetadataTest: SetBlobTest003 start";
-    std::unique_ptr<HeifsMetadata> metadata = ConstructHeifsMetadata(true);
-    ASSERT_NE(metadata, nullptr);
-    std::unique_ptr<uint8_t[]> dst = std::make_unique<uint8_t[]>(MOCK_BUFFER_SIZE);
-    ASSERT_NE(dst, nullptr);
-    ASSERT_EQ(memcpy_s(dst.get(), MOCK_BUFFER_SIZE, MOCK_METADATA, MOCK_BUFFER_SIZE), 0);
-    dst[METADATA_TYPE_INDEX] = static_cast<uint8_t>(MetadataType::EXIF);
-    ASSERT_EQ(metadata->SetBlob(dst.get(), MOCK_BUFFER_SIZE), ERR_IMAGE_INVALID_PARAMETER);
+    std::unique_ptr<HeifsMetadata> heifsMetadata = ConstructHeifsMetadata(true);
+    GifMetadata gifMetadata;
+    gifMetadata.SetValue(GIF_METADATA_KEY_DELAY_TIME, MOCK_DELAY_TIME);
+    uint32_t gifSize = gifMetadata.GetBlobSize();
+    ASSERT_NE(gifSize, 0);
+    std::unique_ptr<uint8_t[]> src = std::make_unique<uint8_t[]>(gifSize);
+    ASSERT_NE(src, nullptr);
+    ASSERT_EQ(gifMetadata.GetBlob(gifSize, src.get()), SUCCESS);
+    ASSERT_EQ(heifsMetadata->SetBlob(src.get(), gifSize), ERR_IMAGE_INVALID_PARAMETER);
     GTEST_LOG_(INFO) << "HeifsMetadataTest: SetBlobTest003 end";
 }
 
 /**
  * @tc.name: SetBlobTest004
- * @tc.desc: Test SetBlob when metadata count is too many.
+ * @tc.desc: Test SetBlob when setting properties with null metadata.
  * @tc.type: FUNC
  */
 HWTEST_F(HeifsMetadataTest, SetBlobTest004, TestSize.Level3)
 {
     GTEST_LOG_(INFO) << "HeifsMetadataTest: SetBlobTest004 start";
-    std::unique_ptr<HeifsMetadata> metadata = ConstructHeifsMetadata(true);
-    ASSERT_NE(metadata, nullptr);
-    std::unique_ptr<uint8_t[]> dst = std::make_unique<uint8_t[]>(MOCK_BUFFER_SIZE);
-    ASSERT_NE(dst, nullptr);
-    ASSERT_EQ(memcpy_s(dst.get(), MOCK_BUFFER_SIZE, MOCK_METADATA, MOCK_BUFFER_SIZE), 0);
-    dst[METADATA_COUNT_INDEX] = MOCK_METADATA_COUNT;
-    ASSERT_EQ(metadata->SetBlob(dst.get(), MOCK_BUFFER_SIZE), ERR_IMAGE_INVALID_PARAMETER);
+    std::unique_ptr<HeifsMetadata> metadataSrc = ConstructHeifsMetadata(true);
+    ASSERT_NE(metadataSrc, nullptr);
+    std::unique_ptr<HeifsMetadata> metadataDst = ConstructHeifsMetadata(false);
+    ASSERT_NE(metadataDst, nullptr);
+    uint32_t size = metadataSrc->GetBlobSize();
+    ASSERT_NE(size, 0);
+    std::unique_ptr<uint8_t[]> src = std::make_unique<uint8_t[]>(size);
+    ASSERT_NE(src, nullptr);
+    ASSERT_EQ(metadataSrc->GetBlob(size, src.get()), SUCCESS);
+    ASSERT_EQ(metadataDst->SetBlob(src.get(), size), SUCCESS);
+    ASSERT_EQ(metadataDst->properties_->size(), 0);
     GTEST_LOG_(INFO) << "HeifsMetadataTest: SetBlobTest004 end";
 }
 
-/**
- * @tc.name: SetBlobTest005
- * @tc.desc: Test SetBlob when metadata key is invalid.
- * @tc.type: FUNC
- */
-HWTEST_F(HeifsMetadataTest, SetBlobTest005, TestSize.Level3)
-{
-    GTEST_LOG_(INFO) << "HeifsMetadataTest: SetBlobTest005 start";
-    std::unique_ptr<HeifsMetadata> metadata = ConstructHeifsMetadata(true);
-    ASSERT_NE(metadata, nullptr);
-    std::unique_ptr<uint8_t[]> dst = std::make_unique<uint8_t[]>(MOCK_BUFFER_SIZE);
-    ASSERT_NE(dst, nullptr);
-    ASSERT_EQ(memcpy_s(dst.get(), MOCK_BUFFER_SIZE, MOCK_METADATA, MOCK_BUFFER_SIZE), 0);
-    dst[METADATA_KEY_INDEX] = ALPHA_E;
-    ASSERT_EQ(metadata->SetBlob(dst.get(), MOCK_BUFFER_SIZE), ERR_IMAGE_INVALID_PARAMETER);
-    GTEST_LOG_(INFO) << "HeifsMetadataTest: SetBlobTest005 end";
-}
-
-/**
- * @tc.name: SetBlobTest006
- * @tc.desc: Test SetBlob when setting properties with null metadata.
- * @tc.type: FUNC
- */
-HWTEST_F(HeifsMetadataTest, SetBlobTest006, TestSize.Level3)
-{
-    GTEST_LOG_(INFO) << "HeifsMetadataTest: SetBlobTest006 start";
-    std::unique_ptr<HeifsMetadata> metadata = ConstructHeifsMetadata(true);
-    ASSERT_NE(metadata, nullptr);
-    ASSERT_EQ(metadata->SetBlob(MOCK_METADATA, MOCK_BUFFER_SIZE), SUCCESS);
-    GTEST_LOG_(INFO) << "HeifsMetadataTest: SetBlobTest006 end";
-}
-
-/**
- * @tc.name: SetBlobTest007
- * @tc.desc: Test SetBlob when setting null properties with metadata.
- * @tc.type: FUNC
- */
-HWTEST_F(HeifsMetadataTest, SetBlobTest007, TestSize.Level3)
-{
-    GTEST_LOG_(INFO) << "HeifsMetadataTest: SetBlobTest007 start";
-    std::unique_ptr<HeifsMetadata> metadata = ConstructHeifsMetadata(false);
-    ASSERT_NE(metadata, nullptr);
-    ASSERT_EQ(metadata->SetBlob(NULL_METADATA, FIXED_BUFFER_SIZE), SUCCESS);
-    GTEST_LOG_(INFO) << "HeifsMetadataTest: SetBlobTest007 end";
-}
-
-/**
- * @tc.name: SetBlobTest008
- * @tc.desc: Test SetBlob when buffer size is smaller than truth.
- * @tc.type: FUNC
- */
-HWTEST_F(HeifsMetadataTest, SetBlobTest008, TestSize.Level3)
-{
-    GTEST_LOG_(INFO) << "HeifsMetadataTest: SetBlobTest008 start";
-    std::unique_ptr<HeifsMetadata> metadata = ConstructHeifsMetadata(false);
-    ASSERT_NE(metadata, nullptr);
-    ASSERT_EQ(metadata->SetBlob(MOCK_METADATA, FIXED_BUFFER_SIZE), ERR_IMAGE_INVALID_PARAMETER);
-    GTEST_LOG_(INFO) << "HeifsMetadataTest: SetBlobTest008 end";
-}
 } // namespace Media
 } // namespace OHOS
