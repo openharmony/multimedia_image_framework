bfcf2368f0032b4f7f92d914892e5c222d18c2a4
fanzhihao8 <fanzhihao6@h-partners.com>
2026-02-10 14:40:53 +0800

    checkbuffersize-1
    Signed-off-by: fanzhihao8 <fanzhihao6@h-partners.com>

--- frameworks/innerkitsimpl/common/src/pixel_map.cpp
+++ frameworks/innerkitsimpl/common/src/pixel_map.cpp
@@ -3260,6 +3260,7 @@ PixelMap *PixelMap::FinishUnmarshalling(PixelMap *pixelMap, Parcel &parcel,
         delete pixelMap;
         return nullptr;
     }
+    pixelMap->isUnmarshalling_ = false;
     return pixelMap;
 }
 
