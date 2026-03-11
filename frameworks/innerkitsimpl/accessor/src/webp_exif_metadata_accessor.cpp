4751eac65b50ded66fcca711d7fbd21fed7dfd8f
chenzhaohui <chenzhaohui14@huawei.com>
2026-03-11 11:49:40 +0800

    add webpexif mutex
    Signed-off-by: chenzhaohui <chenzhaohui14@huawei.com>
    Change-Id: If84580439200a4a2f496d45b0020ee374360acd2

--- frameworks/innerkitsimpl/accessor/src/webp_exif_metadata_accessor.cpp
+++ frameworks/innerkitsimpl/accessor/src/webp_exif_metadata_accessor.cpp
@@ -460,7 +460,7 @@ bool WebpExifMetadataAccessor::WirteChunkVp8x(BufferMetadataStream &bufStream, c
         cond = width <= 0 || height <= 0;
         CHECK_ERROR_RETURN_RET(cond, false);
         byte chunckHeader[] = { 0x56, 0x50, 0x38, 0x58, 0x0a, 0x00, 0x00, 0x00, 0x08,
-                                       0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
+                                0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
         size_t offset = WEBP_CHUNK_HEAD_SIZE;
         uint32_t w = width - 1;
         chunckHeader[offset] = w & WEBP_CHUNK_OPERATE_FLAG;
