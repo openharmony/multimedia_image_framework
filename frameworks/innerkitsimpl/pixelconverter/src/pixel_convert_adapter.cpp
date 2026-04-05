e0e6f14c4761a38ea31143cc5e94595408826d0c
chen-qiao-yi <chenqiaoyi2@huawei.com>
2026-04-05 11:11:23 +0800

    修正 ALPHA_U8
    Signed-off-by: chen-qiao-yi <chenqiaoyi2@huawei.com>

--- frameworks/innerkitsimpl/pixelconverter/src/pixel_convert_adapter.cpp
+++ frameworks/innerkitsimpl/pixelconverter/src/pixel_convert_adapter.cpp
@@ -63,8 +63,8 @@ const std::map<PixelFormat, AVPixelFormat> PixelConvertAdapter::FFMPEG_PIXEL_FOR
 static const map<PixelFormat, SkColorType> PIXEL_FORMAT_MAP = {
     { PixelFormat::UNKNOWN, SkColorType::kUnknown_SkColorType},
     { PixelFormat::ARGB_8888, SkColorType::kRGBA_8888_SkColorType},
-    { PixelFormat::ALPHA_U8, SkColorType::kAlpha_8_SkColorType},
     { PixelFormat::ALPHA_8, SkColorType::kAlpha_8_SkColorType},
+    { PixelFormat::ALPHA_U8, SkColorType::kAlpha_8_SkColorType},
     { PixelFormat::RGB_565, SkColorType::kRGB_565_SkColorType},
     { PixelFormat::RGBA_F16, SkColorType::kRGBA_F16_SkColorType},
     { PixelFormat::RGBA_8888, SkColorType::kRGBA_8888_SkColorType},
