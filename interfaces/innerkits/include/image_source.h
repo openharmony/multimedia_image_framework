241bf025775e0f05c1749f51dca66ee68249cce7
he-guangyao <heguangyao1@h-partners.com>
2026-04-24 10:04:15 +0800

    master-sync-04230922 update010 Signed-off-by heguangyao <heguangyao1@h-partners.com>
    Signed-off-by: he-guangyao <heguangyao1@h-partners.com>

--- interfaces/innerkits/include/image_source.h
+++ interfaces/innerkits/include/image_source.h
@@ -497,7 +497,8 @@ private:
     void DecodeHeifBlobMetadatas(std::unique_ptr<Picture> &picture, std::set<MetadataType> &metadataTypes,
         ImageInfo &info, uint32_t &errorCode);
     void DecodeJpegExtendInfo(std::set<AuxiliaryPictureType> &auxTypes, std::set<MetadataType> &metadataTypes,
-        std::unique_ptr<Picture> &picture, uint32_t &errorCode);
+        std::unique_ptr<Picture> &picture, uint32_t &errorCode,
+        const DownSamplingScaleFactor& downSamplingScaleFactor);
     std::shared_ptr<ImageMetadata> FindMetadataFromMap(MetadataType type);
     std::unique_ptr<PixelMap> GenerateThumbnail(const DecodingOptionsForThumbnail &opts, uint32_t &errorCode);
     std::unique_ptr<PixelMap> DecodeExifThumbnail(const DecodingOptionsForThumbnail &opts,
