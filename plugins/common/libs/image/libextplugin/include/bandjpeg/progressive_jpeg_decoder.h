be4c7c86ad8aaf26978110c6634a678c8c0a6689
hehaokun <hehaokun@noreply.gitcode.com>
2026-04-24 19:48:13 +0800

    update: 更新文件 progressive_jpeg_decoder.h hehaokun1@huawei.com
    Signed-off-by: hehaokun1 <hehaokun1@huawei.com>

--- plugins/common/libs/image/libextplugin/include/bandjpeg/progressive_jpeg_decoder.h
+++ plugins/common/libs/image/libextplugin/include/bandjpeg/progressive_jpeg_decoder.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2023 Huawei Device Co., Ltd.
+ * Copyright (C) 2026 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -12,8 +12,9 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-#ifndef PLUGINS_COMMON_LIBS_IMAGE_LIBEXTPLUGIN_INCLUDE_BANDJPEG_PROGRESSIVE_JPEG_DECODER_H
-#define PLUGINS_COMMON_LIBS_IMAGE_LIBEXTPLUGIN_INCLUDE_BANDJPEG_PROGRESSIVE_JPEG_DECODER_H
+
+#ifndef PLUGINS_COMMON_LIBS_IMAGE_LIBEXTPLUGIN_INCLUDE_PROGRESSIVE_JPEG_DECODER_H
+#define PLUGINS_COMMON_LIBS_IMAGE_LIBEXTPLUGIN_INCLUDE_PROGRESSIVE_JPEG_DECODER_H
 
 #include <cstdint>
 #include <cstddef>
@@ -31,20 +32,66 @@ class InputDataStream;
 class ProgressiveJpegDecoder {
 public:
     struct JpegInputData {
-        uint8_t* buffer = nullptr;
+        uint8_t *buffer = nullptr;
         uint32_t bufferSize = 0;
         std::unique_ptr<uint8_t[]> ownedBuffer = nullptr;
     };
-    using ReadJpegDataFunc = std::function<uint32_t(uint8_t*, uint32_t)>;
+    using ReadJpegDataFunc = std::function<uint32_t(uint8_t *, uint32_t)>;
 
     struct RgbDecodeOptions {
-        SkCodec* codec = nullptr;
+        SkCodec *codec = nullptr;
         SkImageInfo srcInfo;
         SkImageInfo dstInfo;
         OHOS::Media::Size desiredSize = {0, 0};
         OHOS::Media::PixelFormat pixelFormat = OHOS::Media::PixelFormat::UNKNOWN;
-    }
-}
-}
-}
-#endif
+        OHOS::Media::AllocatorType allocatorType = OHOS::Media::AllocatorType::DEFAULT;
+        bool ifSourceCompleted = false;
+        bool supportRegion = false;
+        bool hasOutputBuffer = false;
+        bool hasReusePixelmap = false;
+        bool hasSubset = false;
+        uint32_t sampleSize = 1;
+        int softSampleSize = 1;
+    };
+
+    struct RgbDecodePlan {
+        SkImageInfo dstInfo;
+        uint64_t byteCount = 0;
+        OHOS::Media::PixelFormat pixelFormat = OHOS::Media::PixelFormat::UNKNOWN;
+        bool useDesiredSize = false;
+        bool isRgb888Output = false;
+    };
+
+    struct YuvDecodeOptions {
+        SkCodec *codec = nullptr;
+        OHOS::Media::Size sourceSize = {0, 0};
+        OHOS::Media::Size desiredSize = {0, 0};
+        OHOS::Media::PixelFormat pixelFormat = OHOS::Media::PixelFormat::UNKNOWN;
+        bool ifSourceCompleted = false;
+        bool supportRegion = false;
+        bool hasSubset = false;
+        uint32_t sampleSize = 1;
+        int softSampleSize = 1;
+    };
+
+    struct YuvDecodePlan {
+        OHOS::Media::Size size = {0, 0};
+        uint64_t bufferSize = 0;
+    };
+
+    static uint32_t GetJpegInputData(InputDataStream *stream, const ReadJpegDataFunc &readJpegData,
+        JpegInputData &jpegData);
+    static bool BuildRgbDecodePlan(const RgbDecodeOptions &options, RgbDecodePlan &plan);
+    static uint32_t DecodeRgb(const JpegInputData &jpegData, const RgbDecodePlan &plan,
+        uint8_t *dstPixels, size_t dstStride);
+    static bool BuildYuvDecodePlan(const YuvDecodeOptions &options, YuvDecodePlan &plan);
+    static uint32_t DecodeYuv(const JpegInputData &jpegData, const YuvDecodePlan &plan, DecodeContext &context);
+    static uint64_t GetRgbOutputByteCount(const SkImageInfo &imageInfo, bool isRgb888Format);
+    static uint64_t GetOutputRowStride(const SkImageInfo &imageInfo, const DecodeContext &context,
+        const uint8_t *bufferForDecode);
+    static void ResetDecodeContextPixelsBuffer(DecodeContext &context);
+};
+} // namespace ImagePlugin
+} // namespace OHOS
+
+#endif // PLUGINS_COMMON_LIBS_IMAGE_LIBEXTPLUGIN_INCLUDE_PROGRESSIVE_JPEG_DECODER_H
