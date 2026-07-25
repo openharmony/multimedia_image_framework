a8efaface8c87eda8f0af0e16a60d2a11080c46e
chen-qiao-yi <chenqiaoyi2@h-partners.com>
2026-07-25 17:58:52 +0800

    删除无效行
    Signed-off-by: chen-qiao-yi <chenqiaoyi2@h-partners.com>
    
    AI[0%] Human Fixed[0%] Human[100%] AI Adopted[0%]

--- frameworks/kits/js/common/pixelmap_ndk/pixelmap_native.cpp
+++ frameworks/kits/js/common/pixelmap_ndk/pixelmap_native.cpp
@@ -1611,49 +1611,45 @@ static bool GetHdrMetadata(const OHOS::sptr<OHOS::SurfaceBuffer> &buffer,
         return false;
     }
     switch (key) {
-        case OH_Pixelmap_HdrMetadataKey::HDR_METADATA_TYPE:
-            {
-                CM_HDR_Metadata_Type type;
-                VpeUtils::GetSbMetadataType(buffer, type);
-                if (MetadataNdkTypeMap.find(type) != MetadataNdkTypeMap.end()) {
-                    metadataValue->type = MetadataNdkTypeMap[type];
-                    return true;
-                }
+        case OH_Pixelmap_HdrMetadataKey::HDR_METADATA_TYPE: {
+            CM_HDR_Metadata_Type type;
+            VpeUtils::GetSbMetadataType(buffer, type);
+            if (MetadataNdkTypeMap.find(type) != MetadataNdkTypeMap.end()) {
+                metadataValue->type = MetadataNdkTypeMap[type];
+                return true;
             }
             break;
+        }
         case OH_Pixelmap_HdrMetadataKey::HDR_STATIC_METADATA:
             return GetStaticMetadata(buffer, metadataValue);
-            break;
-        case OH_Pixelmap_HdrMetadataKey::HDR_DYNAMIC_METADATA:
-            {
-                std::vector<uint8_t> dynamicData;
-                if (VpeUtils::GetSbDynamicMetadata(buffer, dynamicData) && (dynamicData.size() > 0)) {
-                    metadataValue->dynamicMetadata.data = (uint8_t*)malloc(dynamicData.size());
-                    if (metadataValue->dynamicMetadata.data == nullptr) {
-                        return false;
-                    }
-                    if (memcpy_s(metadataValue->dynamicMetadata.data,
-                        dynamicData.size(), dynamicData.data(), dynamicData.size()) != EOK) {
-                        free(metadataValue->dynamicMetadata.data);
-                        return false;
-                    }
-                    metadataValue->dynamicMetadata.length = dynamicData.size();
-                    return true;
+        case OH_Pixelmap_HdrMetadataKey::HDR_DYNAMIC_METADATA: {
+            std::vector<uint8_t> dynamicData;
+            if (VpeUtils::GetSbDynamicMetadata(buffer, dynamicData) && (dynamicData.size() > 0)) {
+                metadataValue->dynamicMetadata.data = (uint8_t*)malloc(dynamicData.size());
+                if (metadataValue->dynamicMetadata.data == nullptr) {
+                    return false;
                 }
+                if (memcpy_s(metadataValue->dynamicMetadata.data,
+                    dynamicData.size(), dynamicData.data(), dynamicData.size()) != EOK) {
+                    free(metadataValue->dynamicMetadata.data);
+                    return false;
+                }
+                metadataValue->dynamicMetadata.length = dynamicData.size();
+                return true;
             }
             break;
-        case OH_Pixelmap_HdrMetadataKey::HDR_GAINMAP_METADATA:
-            {
-                std::vector<uint8_t> gainmapData;
-                if (VpeUtils::GetSbDynamicMetadata(buffer, gainmapData) &&
-                    (gainmapData.size() == sizeof(HDRVividExtendMetadata))) {
-                    OH_Pixelmap_HdrGainmapMetadata &dst = metadataValue->gainmapMetadata;
-                    HDRVividExtendMetadata &src = *(reinterpret_cast<HDRVividExtendMetadata*>(gainmapData.data()));
-                    ConvertToOHGainmapMetadata(src, dst);
-                    return true;
-                }
+        }
+        case OH_Pixelmap_HdrMetadataKey::HDR_GAINMAP_METADATA: {
+            std::vector<uint8_t> gainmapData;
+            if (VpeUtils::GetSbDynamicMetadata(buffer, gainmapData) &&
+                (gainmapData.size() == sizeof(HDRVividExtendMetadata))) {
+                OH_Pixelmap_HdrGainmapMetadata &dst = metadataValue->gainmapMetadata;
+                HDRVividExtendMetadata &src = *(reinterpret_cast<HDRVividExtendMetadata*>(gainmapData.data()));
+                ConvertToOHGainmapMetadata(src, dst);
+                return true;
             }
             break;
+        }
         default:
             break;
     }
