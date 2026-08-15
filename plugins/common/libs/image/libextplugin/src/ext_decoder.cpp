7347711e2df82986087e1883a4117b0f6fca041f
hehaokun1 <hehaokun1@huawei.com>
2026-08-15 11:56:09 +0800

    hehaokun1@huawei.com
    Signed-off-by: hehaokun1 <hehaokun1@huawei.com>

--- plugins/common/libs/image/libextplugin/src/ext_decoder.cpp
+++ plugins/common/libs/image/libextplugin/src/ext_decoder.cpp
@@ -2222,7 +2222,6 @@ uint32_t ExtDecoder::ApplyDesiredColorSpaceIfNeeded(DecodeContext &context)
     uint32_t csRet = ApplyDesiredColorSpace(context);
     if (csRet != SUCCESS) {
         IMAGE_LOGE("ApplyDesiredColorSpace failed, err=%{public}u", csRet);
-        return ERR_IMAGE_COLOR_CONVERT;
     }
     return SUCCESS;
 }
